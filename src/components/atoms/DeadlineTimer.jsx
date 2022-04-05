c1e178f5bf21b07e5a8cf50dfd8410d0709b6f72
bmworld <victory289@naver.com>
2022-04-05 21:49:50 +0900

    메인페이지 주문마감배너 추가

--- src/components/atoms/DeadlineTimer.jsx
+++ src/components/atoms/DeadlineTimer.jsx
@@ -1,46 +1,135 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import styled from 'styled-components';
 import rem from './rem';
 
 
-const Dealine_timer = () => {
-  const Wrap = ({children}) => {
-    const Div = styled.div`
-      position:absolute;
-      left:50%;
-      top:50%;
-      min-width:${rem(205)};
-      min-width:${rem(305)};
-      z-index:999;
-      text-align:center;
-      color:#000;
-      font-size:${rem(16)};
-      transform:translateX(-50%);
-      padding: ${rem(10)} ${rem(8)};
-      box-sizing:border-box;
-      background-color:#FFCEBA;
-      border-radius:${rem(8)};
-    `; 
+const bgColor = '#FFCEBA';
+const move_X = rem(36);
 
-    return (
-      <Div className="deadline_timer">{children}</Div>
-    )
+const Rect = styled.i`
+    position: absolute;
+    right: ${move_X};
+    bottom: 3px;
+    transform: translate(0,100%);
+    border-top: 14px solid ${bgColor};
+    border-left: 9px solid transparent;
+    border-right: 9px solid transparent;
+`;
+
+
+const Text = styled.b`
+  font-size: 16px;
+  color: var(--color-main);
+  letter-spacing: 0.8px;
+  white-space:nowrap;
+`;
+
+const NormalText = styled.span`
+  font-size: 16px;
+  color:#000;;
+  white-space:nowrap;
+`;
+
+const Timer = styled.span`
+  margin: 0 ${rem(6)};
+  white-space:nowrap;
+  font-weight:400;
+  width: ${rem(94)};
+  text-align:left;
+`;
+
+
+
+
+Date.prototype.deliveryDeadline = function () {
+  
+  const today = new Date().getDay();
+  let d_Day;
+  switch (today) {
+  case 0: // 일
+      d_Day = 5;
+      break;
+  case 1: // 월
+      d_Day = 4;
+      break;
+  case 2:
+      d_Day = 3;
+      break;
+  case 3:
+      d_Day = 2;
+      break;
+  case 4:
+      d_Day = 1;
+      break;
+  case 5:
+      d_Day = 0; // 주문마감시간: 매주 금요일 23:59:59
+      break;
+  case 6:
+      d_Day = 6;
+      break;
   }
 
 
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const date = now.getDate();
+  const nextDate = date + d_Day + 1; 
+  //279141초 = 3600*24 (일) + 3600 (시간)  + 60 (분) + 
 
-  const Text = styled.b`
-    font-size: 16px;
-    color: var(--color-main);
-    margin-right: 7px;       
+  let DEADLINE = new Date(year, month, nextDate) || "";
+  const gap = Math.floor((DEADLINE.getTime() - now.getTime()) / 1000);
+
+  if(gap < 0) return;
+  
+  // 나머지를 올림해서, 변환
+  const day = d_Day; 
+  const hour = Math.floor( (gap % (60 * 60 * 24))  / (60 * 60)) ; 
+  const min = Math.floor( (gap % ( 60 * 60)) / 60);
+  const sec = Math.floor(gap % 60);
+
+  function convertNum (num) {
+    return num = num < 10 ? '0' + num.toString() : num.toString();
+  }
+
+  
+  return DEADLINE = `${day}일 ${convertNum(hour)}:${convertNum(min)}:${convertNum(sec)}`;
+}   
+           
+
+const Dealine_timer = () => {
+  const [message, setMessage] = useState('');
+
+  const Wrap = styled.div`
+    position:absolute;
+    z-index:999;
+    right: 6px;
+    top:0;
+    transform:translate( ${move_X} , calc(-100% - ${rem(34)}));
+    text-align:center;
+    color:#000;
+    font-size:${rem(16)};
+    padding: ${rem(10)} ${rem(20)};
+    box-sizing:border-box;
+    background-color:${bgColor};
+    border-radius:${rem(8)};
   `;
 
-  const time = new Date().toISOString();
+  
+  useEffect(() => {
+    let timer;
+     timer = setTimeout(() => {
+        setMessage(new Date().deliveryDeadline());
+      }, 1000)
+  })
+
+
   return (
-    <Wrap>
+    <Wrap id='deadline_timer' className="flex-wrap">
       <Text>정기구독배송</Text>
-      <span id='deadline' style={{'marginRight': `${rem(4)}`}}>{time}</span>
-      이후 주문마감!
+      <Timer id='deadline'>{message}</Timer>
+      <NormalText> 이후 주문 마감!</NormalText>
+      <Rect className='rect'/>
     </Wrap>
   )
   
--- src/components/atoms/rem.jsx
+++ src/components/atoms/rem.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-
 
 function rem (px) {
   const browserContenxt = 16;
   const calc = Number(px)/browserContenxt;
-  console.log(calc);
   return calc+'rem';
 }
 
