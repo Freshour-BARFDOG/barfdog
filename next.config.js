13fa52aa7a3c1102a23b73023e403d7a1dec4699
bmworld <victory289@naver.com>
2022-04-25 21:09:51 +0900

    axios : deal with CORS Policy

--- next.config.js
+++ next.config.js
@@ -47,12 +47,18 @@ module.exports = {
   async rewrites() {
     console.log("Rewrites called");
     console.log(process.env.NODE_ENV);
-    return process.env.NODE_ENV !== "production" ? 
-    [
-      {
-        destination: process.env.DESTINATION_URL,
-        source: process.env.SOURCE_PATH,
-      },
-    ] : [];
+    return process.env.NODE_ENV !== "production"
+      ? [
+          {
+            destination: process.env.DESTINATION_URL,
+            source: process.env.SOURCE_PATH,
+          },
+        ]
+      : [
+          {
+            destination: process.env.DESTINATION_URL,
+            source: process.env.SOURCE_PATH,
+          },
+        ];
   },
 };
