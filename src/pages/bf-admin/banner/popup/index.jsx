f27d67df6d8c6f3b19fcf7ccc97c252c73477404
bmworld <victory289@naver.com>
2022-05-04 21:43:15 +0900

    admin > fix build error

--- src/pages/bf-admin/banner/popup/index.jsx
+++ src/pages/bf-admin/banner/popup/index.jsx
@@ -1,62 +1,15 @@
 import React, { Component } from "react";
-import {
-  sortableContainer,
-  sortableElement,
-  sortableHandle,
-  arrayMove
-} from "react-sortable-hoc";
-
 import AdminLayout from "/src/components/admin/AdminLayout";
 import { AdminContentWrapper } from "/src/components/admin/AdminWrapper";
 import MetaTitle from "@src/components/atoms/MetaTitle";
 
 
-
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
 // ! ----------------- 메인배너 페이지에서 순서편집 기능 추가한 뒤에 , 작업 시작 --------- ! //
-
-const DragHandle = sortableHandle(() => <span>::</span>);
-
-const SortableItem = sortableElement(({ value }) => (
-  <li>
-    <DragHandle />
-    {value}
-  </li>
-));
-
-const SortableContainer = sortableContainer(({ children }) => {
-  return <ul>{children}</ul>;
-});
-
-class App extends Component {
-  state = {
-    items: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"],
-  };
-  
-
-  onSortEnd = ({ oldIndex, newIndex }) => {
-    this.setState(({ items }) => ({
-      items: arrayMove(items, oldIndex, newIndex),
-    }));
-  };
-
-  render() {
-    const { items } = this.state;
-
-    return (
-      <SortableContainer onSortEnd={this.onSortEnd} useDragHandle>
-        {items.map((value, index) => (
-          <SortableItem key={`item-${value}`} index={index} value={value} />
-        ))}
-      </SortableContainer>
-    );
-  }
-}
-
 
 
 
