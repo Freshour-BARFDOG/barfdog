52619610cda3c3746091ebf1ee7f2c931b3483b0
bmworld <victory289@naver.com>
2022-05-25 15:39:20 +0900

    admin Reward Page Publishing

--- src/pages/bf-admin/reward/RewardList.jsx
+++ src/pages/bf-admin/reward/RewardList.jsx
@@ -11,10 +11,10 @@ import s from "./reward.module.scss";
     const DATA = {
       id: item.id || 91,
       reg_data: item.reg_data || "22-02-22",
-      name: item.name || "견주 생일 쿠폰",
-      amount: item.amount || "무제한",
+      name: item.name || "친구초대 적립금",
+      amount: item.amount || "3,000원",
       userName: item.userName || "김바프",
-      userId: item.userId || "김바프",
+      email: item.email || "barf@",
       _links: {
         // query_member: {
         //   href: "http://localhost:8080/api/admin/members/91",
@@ -24,22 +24,11 @@ import s from "./reward.module.scss";
 
     return (
       <li className={s.item} key={`item-${DATA.id}`}>
-        <span>{DATA.code}</span>
-        <span>{DATA.sort}</span>
+        <span>{DATA.reg_data}</span>
         <span>{DATA.name}</span>
-        <span className={"text-transform-ellipsis"}>{DATA.description}</span>
-        <span>{DATA.discount}</span>
-        <span>{DATA.couponTarget}</span>
         <span>{DATA.amount}</span>
-        <span>
-          <button
-            className="admin_btn basic_s solid"
-            // onClick={onDeleteItemHandler}
-            // data-apiurl={DATA.apiurl.delete}
-          >
-            삭제
-          </button>
-        </span>
+        <span>{DATA.userName}</span>
+        <span>{DATA.email}</span>
       </li>
     );
   };
