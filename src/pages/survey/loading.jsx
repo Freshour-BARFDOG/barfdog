7dd898571ba751581088630beb70fcba352fb00d
bmworld <victory289@naver.com>
2022-05-25 15:37:38 +0900

    survey page routing

---
+++ src/pages/survey/loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from "/src/components/common/Layout";
+import Wrapper from "/src/components/common/Wrapper";
+import MetaTitle from "@src/components/atoms/MetaTitle";
+
+
+function LoadingPage() {
+  return (
+    <>
+      <MetaTitle title="로딩 중" />
+      <Layout>
+        <Wrapper>로딩 페이지</Wrapper>
+      </Layout>
+    </>
+  );
+}
+
+export default LoadingPage
