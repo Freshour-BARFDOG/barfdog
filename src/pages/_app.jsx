e1a94c15ace9d753e1bbf4eede329a6037b784ff
bmworld <victory289@naver.com>
2022-06-07 18:05:31 +0900

    Google Analytics 설치 및 배포 테스트

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,6 @@ import {ModalContextProvider} from '@store/modal-context';
 import ChannelTalkProvider from "/api/channelTalk/ChannelTalkProvider";
 import GAProvider from "/api/googleAnalytics/GAProvider";
 
-
 // Server Only File (client에서 사용하는 로직 사용불가)
 // Next JS : 최초실행
 //👉 공통된 Data Fetching이 필요하다면 _app.js에 getInitialProps를 붙이면 된다.
