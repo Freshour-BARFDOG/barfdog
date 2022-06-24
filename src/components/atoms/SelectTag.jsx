42a79725fa16bed3e61af8ddd0b8b7d60315c6a3
bmworld <victory289@naver.com>
2022-06-24 14:50:40 +0900

    initial value 기능 보완 (admin > update page)

--- src/components/atoms/SelectTag.jsx
+++ src/components/atoms/SelectTag.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-
-
-const SelectTag = ({ name, id, onChange, options = [], style, ...props }) => {
-  const [selectedValue, setSelectedValue] = useState();
+const SelectTag = ({ name, id, onChange, options = [], initialValue, style, ...props }) => {
+  const [selectedValue, setSelectedValue] = useState(initialValue);
   if (!options.length) return;
 
   const onChangeHandler = (e) => {
     const thisSelect = e.currentTarget;
     const val = thisSelect.value;
     setSelectedValue(val);
-    if (onChange && typeof onChange === "function") onChange(val);
+    if (onChange && typeof onChange === 'function') onChange(val);
   };
+  console.log(selectedValue)
+
 
   return (
     <>
@@ -21,20 +21,16 @@ const SelectTag = ({ name, id, onChange, options = [], style, ...props }) => {
         id={id || name}
         onChange={onChangeHandler}
         style={style}
-        value={selectedValue}
+        value={initialValue} /* to Set Initial Value*/
         {...props}
       >
         {options.map((option, i) => {
-          return i === 0 ? (
+          return (
             <option
-              key={`${option.label}-${i}`}
+              key={`${option}-${i}`}
               value={option.value}
-              defaultValue
+              // selected={initialValue === option.value}
             >
-              {option.label}
-            </option>
-          ) : (
-            <option key={`${option}-${i}`} value={option.value}>
               {option.label}
             </option>
           );
