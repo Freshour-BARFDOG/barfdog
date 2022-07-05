44b813ae917b06548c691c602eb6734e3df440fe
bmworld <victory289@naver.com>
2022-07-05 13:12:00 +0900

    fix bug

--- util/func/transformDate.js
+++ util/func/transformDate.js
@@ -1,19 +1,19 @@
 
 
 
-const transformDate = (d, option='time') => {
+const transformDate = (d, option) => {
   if(!d?.indexOf('-'))return;
   const yy = d.split("-")[0];
   const mm = d.split("-")[1];
   const dd = d.split("-")[2].split("T")[0];
-  const t = d.split("-")[2].split("T")[1];
-  const hour = t.split(':')[0];
-  const min = t.split(':')[1];
-  const sec = Math.floor(t.split(':')[2]);
-  
+
   let result = `${yy}-${mm}-${dd}`;
   
   if(option === 'time'){
+    const t = d?.split("-")[2].split("T")[1];
+    const hour = t?.split(':')[0];
+    const min = t?.split(':')[1];
+    const sec = Math.floor(t?.split(':')[2]);
     result = option === 'time' && `${yy}년 ${mm}월 ${dd}일 ${hour}시 ${min}분 ${sec}초`;
   }
   
