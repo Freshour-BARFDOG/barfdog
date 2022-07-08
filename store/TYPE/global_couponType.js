fabf02d6211db5d970c59ef936f26ff7d4f3283f
bmworld <victory289@naver.com>
2022-07-09 02:38:16 +0900

    add description

--- store/TYPE/global_couponType.js
+++ store/TYPE/global_couponType.js
@@ -3,15 +3,10 @@
 
 
 export const global_couponType = {
-  AUTO_PUBLISHED:'AUTO_PUBLISHED',
-  CODE_PUBLISHED: 'CODE_PUBLISHED',
-  GENERAL_PUBLISHED:'GENERAL_PUBLISHED'
-} //
-/*
-- AUTO_PUBLISHED : 자동발행 / 삭제불가
-- CODE_PUBLISHED :  직접발행 / 유저가 코드 입력 후 쿠폰함에 생성
-- GENERAL_PUBLISHED  : 직접발행 / 어드민이 생성 시, 유저 쿠폰함에 자동 생성시킴 (고객사와 협의 후, 사용하지 않기로 협의된 기능)
-*/
+  AUTO_PUBLISHED:'AUTO_PUBLISHED', // - 자동발행 / 어드민 생성 및 삭제 불가 / 개발자 측에서만 생성가능
+  CODE_PUBLISHED: 'CODE_PUBLISHED', // - 직접발행 / 유저가 코드 입력 후 쿠폰함에 생성
+  // GENERAL_PUBLISHED:'GENERAL_PUBLISHED' //( ! 사용하지 않기로 고객과 협의된 기능) - 직접발행 / 어드민이 생성 시, 유저 쿠폰함에 자동 생성시킴
+}
 
 
 
