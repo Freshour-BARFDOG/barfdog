6cc1b0eb43e46bed6bc25087122cc5c71af1da7f
bmworld <victory289@naver.com>
2022-07-11 17:26:54 +0900

    Review 상태 Type 추가 (, 전체, 요청, 반려, 승인, 관리자)

---
+++ store/TYPE/reviewStateType.js
@@ -0,0 +1,9 @@
+
+
+export const global_reviewStateType = [
+  {ENG:'ALL',KOR:'전체'},
+  {ENG:'REQUEST',KOR:'요청'},
+  {ENG:'RETURN',KOR:'반려'},
+  {ENG:'APPROVAL',KOR:'승인'},
+  {ENG:'ADMIN',KOR:'관리자'},
+]
