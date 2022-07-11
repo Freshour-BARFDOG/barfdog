6d0ca5449f20129a4e8e2344d8cf9dff0eb24b58
bmworld <victory289@naver.com>
2022-07-11 11:57:34 +0900

    CORS 해결 > nextJS ServerSide Request & Response

--- api/axios.config.js
+++ api/axios.config.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL_DEV;
-axios.defaults.headers.post["Content-Type"] = axios.defaults.headers.post[
-  "Content-Type"
-] = "application/x-www-form-urlencoded";
 axios.defaults.withCredentials = true;
 
 
