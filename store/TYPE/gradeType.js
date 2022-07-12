6c20699792b071eb95fdc9fbe54eee3cf9111ec5
bmworld <victory289@naver.com>
2022-07-12 11:42:01 +0900

    modified type : 다이아 -> 다이아몬드

--- store/TYPE/gradeType.js
+++ store/TYPE/gradeType.js
@@ -1,3 +1,3 @@
 
-export const global_gradeType = ['브론즈','실버','골드','플래티넘','다이아','더바프'] ;
+export const global_gradeType = ['브론즈','실버','골드','플래티넘','다이아몬드','더바프'] ;
 export const global_gradeType_ENG =  ['BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND', 'BARF']
