b74c5ee7541217d0b8599f7f28fe94ce0814400d
bmworld <victory289@naver.com>
2022-07-25 04:07:16 +0900

    bug fix & delete unnecessary code

--- util/func/transformPhoneNumber.js
+++ util/func/transformPhoneNumber.js
@@ -1,5 +1,6 @@
 export const
   transformPhoneNumber = (phoneNum) => {
+      if(!phoneNum)return;
     const RegExp = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/;
     const error = !RegExp.test( phoneNum );
     if ( error ) return console.error( '입력받은 휴대전호번호의 형식이 올바르지 않습니다.' );
