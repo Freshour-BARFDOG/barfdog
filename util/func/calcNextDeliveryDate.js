636e20db95824dc3ba6b94f38e268c93c179d22c
bmworld <victory289@naver.com>
2022-07-28 02:23:49 +0900

    add function > 바프독 정기배송일자

---
+++ util/func/calcNextDeliveryDate.js
@@ -0,0 +1,15 @@
+import transformDate, {transformToday} from './transformDate';
+
+export const calcNextDeliveryDate = (d = transformToday(), unit = '월일') => {
+  // WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const FRI = 5; // 금
+  const next_WED = 10;
+  const theWeekAfterNext_WED = 17;
+  const today = new Date( d ); //
+  const dayOfWeek = today.getDay(); // num // 0 ~ 6
+  const sundayOfWeek = today.getDate() - dayOfWeek; // 이번 주의 첫 번째 일 (SUN)
+  const diff = sundayOfWeek + (dayOfWeek <= FRI ? next_WED : theWeekAfterNext_WED);
+  const nextDeliveryDate = new Date( today.setDate( diff ) ).toISOString().substring( 0, 10 );
+  // console.log(new Date(today.setDate(diff)).toISOString().substring(0,10));
+  return transformDate( nextDeliveryDate, unit );
+};
