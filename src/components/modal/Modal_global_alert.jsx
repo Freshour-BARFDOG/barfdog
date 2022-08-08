e442fef20f486afe5c669acf186e766dc18d2939
bmworld <victory289@naver.com>
2022-08-09 06:21:22 +0900

    bug fix: component의 mount & unmount 상태와 관계없이 window scroll 상태유지

--- src/components/modal/Modal_global_alert.jsx
+++ src/components/modal/Modal_global_alert.jsx
@@ -5,47 +5,52 @@ import ModalWrapper from './ModalWrapper';
 
 
 
-function Modal_global_alert({ message, onClick, background,   ...props }) {
+function Modal_global_alert({
+  message,
+  onClick,
+  background,
+  ...props
+}) {
   const mct = useModalContext();
   const modalState = mct.hasAlert;
   const modalContextMessage = mct.message;
   const [style, setStyle] = useState({});
-  const [targetScrollYPos, setTargetScrollYPos] = useState( null );
-  
+  const [targetScrollYPos, setTargetScrollYPos] = useState(null);
+
   useEffect(() => {
     callbackAfterAnimation(modalState);
     const scrollYPos = window.scrollY;
-    if(modalState && scrollYPos){
+    if (modalState && scrollYPos) {
       document.body.style.cssText = `
       position:fixed;
       top : -${scrollYPos}px;
     `;
-      setTargetScrollYPos(scrollYPos)
+      setTargetScrollYPos(scrollYPos);
       window.scrollTo(0, parseInt(scrollYPos || targetScrollYPos));
     } else {
       // document.body.style.cssText = ``;
       // window.scrollTo(0, parseInt(scrollYPos || targetScrollYPos));
     }
-    return (()=>{
+    return () => {
       document.body.style.cssText = ``;
-    })
+      window.scrollTo(0, parseInt(scrollYPos || targetScrollYPos)); // alert가 unmounted 되고,  body의 fixed styled이 사라지면서, scrollY position이 화면 최상단으로 변경되는 것을 막음
+    };
   }, [modalState]);
-  
 
   const callbackAfterAnimation = (modalState) => {
     const delay = modalState ? 0 : 500;
     setTimeout(() => {
-      setStyle({ display: modalState ? "block" : "none" });
+      setStyle({ display: modalState ? 'block' : 'none' });
     }, delay);
   };
 
-  const onClickHandler = ()=>{
-    if(onClick && typeof onClick==='function'){
+  const onClickHandler = () => {
+    if (onClick && typeof onClick === 'function') {
       onClick();
     } else {
       mct.alertHide();
     }
-  }
+  };
 
   return (
     <>
