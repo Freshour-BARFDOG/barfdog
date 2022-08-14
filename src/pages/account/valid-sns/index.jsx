a7dd8ce89d60e593638f0a2df66f85410d871b37
bmworld <victory289@naver.com>
2022-08-15 03:05:55 +0900

    small change > change method name, set prettier

--- src/pages/account/valid-sns/index.jsx
+++ src/pages/account/valid-sns/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import s from 'src/pages/account/valid-sns/valid-sns.module.scss';
 import MetaTitle from '/src/components/atoms/MetaTitle';
 import Layout from '/src/components/common/Layout';
@@ -7,12 +7,12 @@ import ErrorMessage from '/src/components/atoms/ErrorMessage';
 import filter_emptyValue from '/util/func/filter_emptyValue';
 import axios from 'axios';
 import Modal_global_alert from '/src/components/modal/Modal_global_alert';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Spinner from '/src/components/atoms/Spinner';
-import {useModalContext} from "/store/modal-context";
-import enterKey from "/util/func/enterKey";
-import {authAction} from "/store/auth-slice";
-
+import { useModalContext } from '/store/modal-context';
+import enterKey from '/util/func/enterKey';
+import { authAction } from '/store/auth-slice';
+import { filter_blindEmail } from '../../../../util/func/filter_blindEmail';
 
 export default function ValidSnsPage() {
   const userState = useSelector((s) => s.userState);
@@ -21,17 +21,18 @@ export default function ValidSnsPage() {
   const [alertModalMessage, setAlertModalMessage] = useState('');
   const [formValues, setFormValues] = useState({});
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState( false );
-  const [tokenFromServer, setTokenFromServer] = useState( null );
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [tokenFromServer, setTokenFromServer] = useState(null);
   const [isLoading, setIsLoading] = useState({});
-  
-  useEffect( () => {
-    if(!userState.snsInfo.provider || !userState.snsInfo.provider){
+
+  // console.log(userState)
+  useEffect(() => {
+    if (!userState.snsInfo.provider || !userState.snsInfo.provider) {
       alert('연동할 SNS정보가 없습니다.');
-      window.location.href= '/';
+      // window.location.href= '/';
     }
-  }, [] );
-  
+  }, []);
+
   const onChangeHandler = (e) => {
     const { id, value } = e.currentTarget;
     let filteredValue = filter_emptyValue(value);
@@ -45,12 +46,13 @@ export default function ValidSnsPage() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    if(isSubmitted)return console.error('이미 제출된 양식입니다.');
-    if(!formValues.password){ // client 유효성검사: 비밀번호가 비어있는지
+    if (isSubmitted) return console.error('이미 제출된 양식입니다.');
+    if (!formValues.password) {
+      // client 유효성검사: 비밀번호가 비어있는지
       mct.alertShow();
       return setAlertModalMessage(`비밀번호를 입력해주세요.`);
     }
-  
+
     // const body = { // ! TEST TESTTESTTESTTESTTESTTEST
     //   password: 'user',
     //   phoneNumber: '01099038544',
@@ -59,8 +61,7 @@ export default function ValidSnsPage() {
     //   tokenValidDays: null, // null 일 경우, 서버 최소 토큰유지: 2시간
     // };
     // console.log(body);
-    
-    
+
     const body = {
       password: formValues.password,
       phoneNumber: userState.snsInfo.mobile.replace(/-/g, '') || userState.snsInfo.mobile_e164, // 본인 휴대폰번호 ! Q. 국제번호일경우 => 처리 방침 ?
@@ -68,8 +69,8 @@ export default function ValidSnsPage() {
       providerId: userState.snsInfo.providerId, // sns 고유값
       tokenValidDays: null, // null 일 경우, 서버 최소 토큰유지: 2시간
     };
-    console.log('valid_body:', body)
-    
+    console.log('valid_body:', body);
+
     try {
       setIsLoading((prevState) => ({
         ...prevState,
@@ -94,7 +95,7 @@ export default function ValidSnsPage() {
         .catch((err) => {
           mct.alertHide();
           // console.log(err.response);
-          if(err.response.status === 400){
+          if (err.response.status === 400) {
             mct.alertShow();
             setAlertModalMessage(`비밀번호가 올바르지 않습니다.`);
           }
@@ -108,36 +109,30 @@ export default function ValidSnsPage() {
       submit: false,
     }));
   };
-  
+
   const onEnterKeyHandler = (e) => {
     enterKey(e, onSubmit);
   };
-  
-  
-  const onGlobalModalCallback = () => {
+
+  const onConfirmHandler = () => {
     mct.alertHide();
   };
-  
-  const GlobalModalSuccessCallback = ()=>{
+
+  const onSuceessHandler = () => {
     mct.alertHide();
     const token = tokenFromServer;
-    if(token){
+    if (token) {
       dispatch(authAction.login({ token, redirect: '/account/valid-sns/result' }));
     } else {
       alert('인증과정 중에 문제가 발생했습니다. 다시 시도해주세요.');
-      window.location.href ='/account/login'
+      window.location.href = '/account/login';
     }
-    
-  }
-  
+  };
 
-  if(!userState.snsInfo.providerId || !userState.snsInfo.provider){
-    return console.error('Required Provider')
+  if (!userState.snsInfo.providerId || !userState.snsInfo.provider) {
+    return console.error('Required Provider');
   }
 
-
-  
-  
   return (
     <>
       <MetaTitle title="SNS계정 연동확인" />
@@ -146,7 +141,7 @@ export default function ValidSnsPage() {
           <div className={s.box}>
             <p className={s.text}>
               고객님은 기존에 가입된 회원입니다.
-              <br />
+              <p>{filter_blindEmail(userState.snsInfo.email)}</p>
               기존 계정의 비밀번호 입력 후 연동이 완료됩니다.
             </p>
             <div className={s.container}>
@@ -169,7 +164,14 @@ export default function ValidSnsPage() {
           </div>
         </Wrapper>
       </Layout>
-      <Modal_global_alert message={alertModalMessage} onClick={isSubmitted ? GlobalModalSuccessCallback : onGlobalModalCallback}/>
+      <Modal_global_alert
+        message={alertModalMessage}
+        onClick={isSubmitted ? onSuceessHandler : onConfirmHandler}
+      />
     </>
   );
 }
+
+
+
+
