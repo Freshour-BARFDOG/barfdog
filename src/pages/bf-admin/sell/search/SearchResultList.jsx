024452fad06211aa94b26daf02b722c9c050da36
bmworld <victory289@naver.com>
2022-08-17 10:07:56 +0900

    admin > 판매관리 > 검색결과 리스트 change column

--- src/pages/bf-admin/sell/search/SearchResultList.jsx
+++ src/pages/bf-admin/sell/search/SearchResultList.jsx
@@ -1,28 +1,32 @@
 import s from './search.module.scss';
-import Checkbox from '@src/components/atoms/Checkbox';
 import transformDate from '/util/func/transformDate';
 import { orderStatus } from '/store/TYPE/orderStatusTYPE';
-import {transformPhoneNumber} from "/util/func/transformPhoneNumber";
-import popupWindow from "/util/func/popupWindow";
+import { transformPhoneNumber } from '/util/func/transformPhoneNumber';
+import popupWindow from '/util/func/popupWindow';
+import PureCheckbox from '/src/components/atoms/PureCheckbox';
+import React from 'react';
 
-export default function SearchResultList({ items }) {
+export default function SearchResultList({ items}) {
   if (!items || !items.length) return;
 
   return (
     <ul className="table_body">
       {items.map((item, i) => (
-        <Item key={`item-${item.id}-${i}`} index={i} item={item} />
+        <Item
+          key={`item-${item.id}-${i}`}
+          index={i}
+          item={item}
+        />
       ))}
     </ul>
   );
 }
 
-const Item = ({ item, sortableItemRef, index }) => {
-  
+const Item = ({ item, sortableItemRef}) => {
   const DATA = {
     id: item.id, // 주문 id => ! 주문 id로 주문정보를 조회가능
     orderItemId: item.orderItemId, // 주문한 상품의 id
-    merchantUid: item.merchantUid, // 상품 주문 번호
+    // merchantUid: item.merchantUid, // 상품 주문 번호 // ! 개별 상품 취소 기능 삭제로 인하여, 해당 column 미노출
     orderStatus: orderStatus.KOR[item.orderStatus],
     orderDate: transformDate(item.orderDate, 'time', { seperator: '/' }),
     orderType: item.orderType,
@@ -33,26 +37,24 @@ const Item = ({ item, sortableItemRef, index }) => {
     recipientPhoneNumber: transformPhoneNumber(item.recipientPhoneNumber),
     bundleStatus: item.packageDelivery ? 'Y' : 'N',
   };
-  
-  
+
   const onPopupHandler = (e) => {
     e.preventDefault();
-    if (typeof window === "undefined") return;
+    if (typeof window === 'undefined') return;
     const href = `/bf-admin/sell/popup/${DATA.orderType}/${DATA.id}`;
     popupWindow(href, { width: 1000, height: 716 });
   };
-  
-  
+
   return (
     <li className={s.item} ref={sortableItemRef} data-idx={DATA.id}>
-      <span>
-        <Checkbox
-          id="reviewId"
-          onClick={(value) => {
-            console.log(value);
-          }}
-        />
-      </span>
+      {/*<span>*/}
+      {/*  <PureCheckbox*/}
+      {/*    id={item.id}*/}
+      {/*    className={s.inner}*/}
+      {/*    onClick={onSelectedItem}*/}
+      {/*    value={selectedIdList.indexOf(item.id) >= 0 || ''}*/}
+      {/*  />*/}
+      {/*</span>*/}
       <span>
         <button
           className="admin_btn basic_s solid"
@@ -65,9 +67,9 @@ const Item = ({ item, sortableItemRef, index }) => {
       <span>
         <em className={'overflow-x-scroll'}>{DATA.orderItemId}</em>
       </span>
-      <span>
-        <em className={'overflow-x-scroll'}>{DATA.merchantUid}</em>
-      </span>
+      {/*<span>*/}
+      {/*  <em className={'overflow-x-scroll'}>{DATA.merchantUid}</em>*/}
+      {/*</span>*/}
       <span>
         <em className={'overflow-x-scroll'}>
           <p>{DATA.orderStatus}</p>
