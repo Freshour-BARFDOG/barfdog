d5dc17318eca4123fdb51deb4d71b602b261d751
barfdogOwner <oilmanj@naver.com>
2022-08-18 21:50:40 +0900

    check env

--- src/pages/api/axios.config.js
+++ src/pages/api/axios.config.js
@@ -8,6 +8,9 @@ axios.defaults.baseURL =
     : process.env.NEXT_PUBLIC_API_URL_DEV;
 axios.defaults.withCredentials = true;
 
+console.log('process.env.NODE_ENV  :',process.env.NODE_ENV);
+console.log('axios.defaults.baseURL:',axios.defaults.baseURL)
+console.log('process.env.NODE_ENV === production ?:',process.env.NODE_ENV === 'production')
 // * axiosConfig > 유저 & ADMIN의 config이 동일하다 => 서버에서 토큰의 권한을 자체 검증하기 떄문에
 // CLIENT에서 아래와 같이 처리할 필요가 없다
 // 아래 코드 중에서 아무거나 골라써도 무방하다.
