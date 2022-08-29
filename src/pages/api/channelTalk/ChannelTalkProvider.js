61aa0b257bbabc1512d79dcb1260c8a924be9c4f
sbkim <victory289@naver.com>
2022-08-29 18:35:35 +0900

    채널톡 api > 작업 중

--- src/pages/api/channelTalk/ChannelTalkProvider.js
+++ src/pages/api/channelTalk/ChannelTalkProvider.js
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import ChannelTalkService from "./ChannelTalkService";
-
+import axios from 'axios';
 
 
 const ChannelTalkProvider = ({children}) => {
@@ -10,14 +10,16 @@ const ChannelTalkProvider = ({children}) => {
 
   const router = useRouter();
   const dispatch = useDispatch();
-
-  const ADMIN_BASE_PATH_KEY = "bf-admin";
-  const DISABLED_PATH = ['/popup'];
-  const user = {isLoggedIn:false } // MEMO ______FOR TEST Info
-  const {isLoggedIn} = user;
-
-
+  const auth = useSelector(s=>s.auth);
+  const userInfo = auth.userInfo;
+  const isLoggedIn = userInfo?.email;
+  
+  
+  
+  
   useEffect(() => {
+    const ADMIN_BASE_PATH_KEY = "bf-admin";
+    const DISABLED_PATH = ['/popup'];
     const isAdminPath = router.asPath.split("/")[1] === ADMIN_BASE_PATH_KEY;
     let isDisabledPath;
     DISABLED_PATH.map(path=>{
@@ -28,17 +30,16 @@ const ChannelTalkProvider = ({children}) => {
     // MEMO ChannelTalk: 사용자 Path에 설치  (Admin페이지: 적용 안 함)
     if (isAdminPath || isDisabledPath) return;
     const channelTalk = new ChannelTalkService();
-
-
+  
     // TODO 유저로그인 기능 추가 후 , ChannelTalk 유저정보 연동기능 (채널톡에 유저정보를 넘겨준다)
     if(isLoggedIn) {
       channelTalk.boot({
         pluginKey: process.env.NEXT_PUBLIC_CHANNEL_IO_KEY,
         memberId: uid,
         profile: {
-          name,
-          // mobileNumber,
-          // email,
+          name: userInfo.name,
+          mobileNumber: userInfo.phoneNumber,
+          email: userInfo.email,
           // avatarUrl,
         },
       });
@@ -52,7 +53,7 @@ const ChannelTalkProvider = ({children}) => {
       channelTalk.shutdown();
     };
 
-  }, [dispatch, router, user]);
+  }, [dispatch, router, userInfo]);
 
 
 
@@ -64,3 +65,21 @@ const ChannelTalkProvider = ({children}) => {
 };
 
 export default ChannelTalkProvider;
+
+
+
+
+const getApiData = async (userId) => {
+  const apiUrl = `https://api.channel.io/open/v5/users/${userId}/user-chats?sortOrder=desc&limit=25`
+  const accessKey = '621f1bbdc6f9d0337bcc';
+  const accessSecretKey = '6ef92b0a1c50f27ee7008255e5b23685';
+  const config = {
+    headers:{
+      'Content-Type': 'application/json',
+      'x-access-key': accessKey,
+      'x-access-secret': accessSecretKey
+    }
+  }
+  const res = await axios.get(apiUrl,config);
+  console.log(res);
+}
