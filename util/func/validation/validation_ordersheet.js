7313c2b008f065b11e880136f073fb01d7600800
sbkim <victory289@naver.com>
2022-08-31 20:31:38 +0900

    bug fix: 일반결제 시, 묶음배송여부에 따른 validation key값 변경

--- util/func/validation/validation_ordersheet.js
+++ util/func/validation/validation_ordersheet.js
@@ -43,6 +43,31 @@ export const validate = (obj) => {
 };
 
 
+export const validateInBundleDelivery = (obj) => {
+  let errors = {};
+  
+  const keys = Object.keys(obj);
+  
+  for (const key of keys) {
+    const val = obj[key];
+    
+    switch (key) {
+      case 'paymentMethod':
+        errors[key] = valid_isEmpty(val);
+        break;
+      case 'agreePrivacy':
+        errors[key] = valid_isEmpty(val);
+        break;
+      case 'paymentPrice':
+        errors[key] = valid_paymentPrice(val);
+        break;
+    }
+  }
+  console.log('Valid Result (formValues) : ', errors);
+  return errors;
+};
+
+
 const valid_paymentPrice = (val) =>{
   let error='';
   if(typeof val !== 'number') {
