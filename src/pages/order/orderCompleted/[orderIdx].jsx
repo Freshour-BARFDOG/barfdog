26966a2e4bf694c166256a8062778d5b8f02f497
u0choi <u0choi@binter.co.kr>
2022-09-02 15:50:20 +0900

    일반 주문 완료 페이지 셋팅 오류 수정

--- src/pages/order/orderCompleted/[orderIdx].jsx
+++ src/pages/order/orderCompleted/[orderIdx].jsx
@@ -147,8 +147,8 @@ export async function getServerSideProps(ctx) {
   const { orderIdx , imp_uid, merchant_uid, imp_success,error_msg} = query;
   
   console.log(query);
-  let orderItemValue;
-  let address;
+  let orderItemValue='';
+  let address='';
 
   if(imp_success == 'true'){
     console.log(merchant_uid);
@@ -160,17 +160,6 @@ export async function getServerSideProps(ctx) {
     });
 
     console.log(r);
-  
-    const getApiUrl = `/api/orders/${orderIdx}/general`; // API 검색어: 일반 주문 하나 조회
-
-    let res = await getDataSSR(req, getApiUrl);
-    const data = res?.data;
-    const itemList = data.orderItemDtoList;
-
-    if (data) {
-      orderItemValue = `${itemList[0].itemName} ${itemList.length > 1 ? `외 ${itemList.length-1}개` :''}`;
-      address = `${data.orderDto.street} ${data.orderDto.detailAddress}`;  
-    }
 
   } else if(imp_success == 'false'){
      // 모바일 결제 실패
@@ -180,7 +169,18 @@ export async function getServerSideProps(ctx) {
       // 임시로 넣은 코드 :  결제취소시 , 전역에 import 결제 html이 잔류하여, 없애기위한 용도
       // window.location.href= '/';
     }
-    
+  }
+
+  const getApiUrl = `/api/orders/${orderIdx}/general`; // API 검색어: 일반 주문 하나 조회
+
+  let res = await getDataSSR(req, getApiUrl);
+  console.log(res);
+  const data = res?.data;
+  const itemList = data.orderItemDtoList;
+  
+  if (data) {
+    orderItemValue = `${itemList[0].itemName} ${itemList.length > 1 ? `외 ${itemList.length-1}개` :''}`;
+    address = `${data.orderDto.street} ${data.orderDto.detailAddress}`;  
   }
   
   return { props: { orderIdx, orderItemValue, address } };
