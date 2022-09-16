821e645f3c4508cc66a828a1bfd59cf3a24dc289
sbkim <victory289@naver.com>
2022-09-16 20:11:16 +0900

    GA 등록기능 추가 중

--- src/pages/bf-admin/dashboard/index.jsx
+++ src/pages/bf-admin/dashboard/index.jsx
@@ -10,9 +10,6 @@ import Spinner from '/src/components/atoms/Spinner';
 import { getData } from '/src/pages/api/reqData';
 import transformDate, { transformToday } from '/util/func/transformDate';
 import { orderStatus } from '/store/TYPE/orderStatusTYPE';
-import axios from "axios";
-
-
 
 export default function DashboardPage() {
 
@@ -25,15 +22,15 @@ export default function DashboardPage() {
   const [term, setTerm] = useState(initialTerm);
   const [isLoading, setIsLoading] = useState(false);
   const [info, setInfo] = useState({});
-  
-  console.log(info)
+
   
   useEffect(()=>{
     //구글 analytics 방문자정보 가져오기
+    
     (async ()=>{
-      
       try {
-      
+        const res = await getData('http://localhost:4000/api/getSelfCookie2');
+        console.log('res:' ,res)
       } catch (err) {
           console.error(err)
       }
@@ -135,7 +132,7 @@ export default function DashboardPage() {
           ...prevState,
           ga: true,
         }));
-        const visitorCount = 100;
+        const visitorCount = 0;
         DATA = {
           ...DATA,
           statistics: {
@@ -168,12 +165,21 @@ export default function DashboardPage() {
       diffDate: diffDate
     });
   };
+  
+  
+  const googleLogin = () => {
+    authorize();
+  }
+  
 
   return (
     <>
+      <script src="https://apis.google.com/js/api.js" type='text/javascript'></script>
+      <script src="https://apis.google.com/js/client.js?onload=authorize" type='text/javascript'></script>
       <MetaTitle title="대시보드" admin={true} />
       <AdminLayout>
         <AdminContentWrapper className={s.wrapper}>
+     
           <h1 className={`${s['title']} title_main`}>
             {isLoading.fetching ? <Spinner /> : '대시보드'}
           </h1>
@@ -187,6 +193,7 @@ export default function DashboardPage() {
                   messagePosition={'left'}
                   className={s.tooltip}
                 />
+                <button type={'button'} className={'admin_btn basic_m solid'} onClick={googleLogin}>구글로그인</button>
               </h2>
             </div>
             <div className={s['cont-section']}>
@@ -326,6 +333,138 @@ export default function DashboardPage() {
   );
 }
 
+
+
+function authorize() {
+  // Handles the authorization flow.
+  // `immediate` should be false when invoked from the button click.
+  const CLIENT_ID = '977291702808-drjsc474igbigifrjsq2kbm0endo7o6p.apps.googleusercontent.com'; // <-- 발급받은 Client ID 입력 (https://console.cloud.google.com/)
+  
+  // Set authorized scope.
+  const SCOPES = ['https://www.googleapis.com/auth/analytics.readonly'];
+  
+  const authData = {
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    immediate: false
+  };
+  
+  gapi.auth.authorize(authData, function (response) {
+    console.log(response)
+    
+    if (response.error) {
+      alert('로그인 에러가 있습니다.')
+    }
+    else {
+      queryAccounts();
+    }
+  });
+}
+
+function queryAccounts() {
+  // Load the Google Analytics client library.
+  gapi.client.load('analytics', 'v3').then(function () {
+    
+    // Get a list of all Google Analytics accounts for this user
+    gapi.client.analytics.management.accounts.list().then(handleAccounts);
+  });
+}
+
+function handleAccounts(response) {
+  console.log(response)
+  // Handles the response from the accounts list method.
+  if (response.result.items && response.result.items.length) {
+    // Get the first Google Analytics account.
+    const firstAccountId = response.result.items[0].id;
+    
+    // Query for properties.
+    queryProperties(firstAccountId);
+  } else {
+    console.log('No accounts found for this user.');
+  }
+}
+
+function queryProperties(accountId) {
+  // Get a list of all the properties for the account.
+  gapi.client.analytics.management.webproperties.list(
+    { 'accountId': accountId })
+    .then(handleProperties)
+    .then(null, function (err) {
+      // Log any errors.
+      console.log(err);
+    });
+}
+
+function handleProperties(response) {
+  // Handles the response from the webproperties list method.
+  if (response.result.items && response.result.items.length) {
+    
+    // Get the first Google Analytics account
+    const firstAccountId = response.result.items[0].accountId;
+    
+    // Get the first property ID
+    const firstPropertyId = response.result.items[0].id;
+    
+    // Query for Views (Profiles).
+    queryProfiles(firstAccountId, firstPropertyId);
+  } else {
+    console.log('No properties found for this user.');
+  }
+}
+
+function queryProfiles(accountId, propertyId) {
+  // Get a list of all Views (Profiles) for the first property
+  // of the first Account.
+  gapi.client.analytics.management.profiles.list({
+    'accountId': accountId,
+    'webPropertyId': propertyId
+  })
+    .then(handleProfiles)
+    .then(null, function (err) {
+      // Log any errors.
+      console.log(err);
+    });
+}
+
+function handleProfiles(response) {
+  // Handles the response from the profiles list method.
+  if (response.result.items && response.result.items.length) {
+    // Get the first View (Profile) ID.
+    const firstProfileId = response.result.items[0].id;
+    // Query the Core Reporting API.
+    queryCoreReportingApi(firstProfileId);
+    
+  } else {
+    console.log('No views (profiles) found for this user.');
+  }
+}
+
+function queryCoreReportingApi(profileId) {
+  // Query the Core Reporting API for the number sessions for
+  // the past seven days.
+  gapi.client.analytics.data.ga.get({
+    'ids': 'ga:' + profileId,
+    // ## 조회 시작일자
+    'start-date': '2020-03-03',
+    // ## 조회 마지막일자
+    'end-date': '2020-03-09',
+    // ##  -- 사용자, 신규 방문자, 세션, 이탈률, 평균세션시간(초), 페이지뷰 수, 세션당 페이지수, 사용자당 세션 수
+    'metrics': 'ga:users,ga:newUsers,ga:sessions,ga:bounceRate,ga:avgSessionDuration,ga:pageviews,ga:pageviewsPerSession,ga:sessionsPerUser',
+    // ##  -- 소스 , 매체
+    'dimensions': 'ga:source,ga:medium'
+  })
+    .then(function (response) {
+      var formattedJson = JSON.stringify(response.result, null, 2);
+      document.getElementById('query-output').value = formattedJson;
+    })
+    .then(null, function (err) {
+      // Log any errors.
+      console.log(err);
+    });
+}
+
+
+
 const DUMMY_RESPONSE = {
   data: {
     newOrderCount: 21,
@@ -395,3 +534,8 @@ const DUMMY_RESPONSE = {
     },
   },
 };
+
+
+
+
+
