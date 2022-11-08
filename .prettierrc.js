58376e1a0b2b42a5289c9768fd2a9782d50601de
sbkim <victory289@naver.com>
2022-11-08 16:26:03 +0900

    Set code format by Prettier
    - Added code format to maintain common code style

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports =  {
-  semi:  true,
-  trailingComma:  'all',
-  singleQuote:  true,
-  printWidth:  100,
-  tabWidth:  2,
+  "printWidth": 80,
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "bracketSpacing": true,
+  "arrowParens": "always",
+  "endOfLine": "auto",
+  "react/jsx-max-props-per-line": [1, { "when": "always" }]
 };
