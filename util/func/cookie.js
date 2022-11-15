55e05efc38b15b0188b060d86f0a5a2660c594b6
sbkim <victory289@naver.com>
2022-11-15 09:08:42 +0900

    Refactoring
    - Changed setTime Method from switch statement to object
    
    - Tests were conducted in the Chrome browser to test that cookie time worked correctly.

--- util/func/cookie.js
+++ util/func/cookie.js
@@ -1,39 +1,37 @@
+// # cf. 쿠키테스트 환경
+//  Chrome: 정상작동 (getTime으로 설정할 경우, 365일 설정가능)
+//  BraveBrowser: 위의 크롬과 동일한 조건에서 '최대 7일'까지 설정 가능
 
 
-const option = {path: '/'}
+export const setCookie = function (
+  name,
+  value,
+  dateUnit,
+  expNumber,
+  option = { path: '/' },
+) {
+ 
+  const expiryTime = {
+    date: expNumber * 24 * 60 * 60 * 1000,
+    hour: expNumber * 60 * 60 * 1000,
+    min: expNumber * 60 * 1000,
+    sec: expNumber * 1000,
+    def: expNumber * 24 * 60 * 60 * 1000,
+  };
 
-export const setCookie = function (name, value, dateUnit, expNumber, option={path: '/'}) {
-  const date = new Date();
-  let expiryTime;
-  switch (dateUnit){
-    case 'date':
-      expiryTime = expNumber * 24 * 60 * 60 * 1000;
-    break;
-    case 'hour':
-      expiryTime = expNumber * 60 * 60 * 1000;
-    break;
-    case 'min':
-      expiryTime = expNumber * 60 * 1000;
-      break;
-    case 'sec':
-      expiryTime = expNumber * 1000;
-      break;
-    default:
-      expiryTime = 24 * 60 * 60 * 1000; // 만료시간  기본 1일 후
-  }
-  date.setTime(date.getTime() + expiryTime);
-  document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + `;path=${option.path}`;
-  // console.log('쿠키 만료예정:  +', expNumber, dateUnit)
+  const today = new Date();
+  today.setTime(today.getTime() + expiryTime[dateUnit || 'def']);
+  document.cookie = `${name}=${value};expires=${today.toUTCString()};path=${
+    option.path
+  }`;
+  // console.log('쿠키 만료예정\n','CookieName:',name, '\nCookieDate: ',today, expNumber, dateUnit)
 };
 
-
-
 export const getCookie = function (name) {
-  const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+  const value = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
   return value ? value[2] : null;
 };
 
-
-export const deleteCookie = function (name,option={path:'/'}) {
-  document.cookie = `${name}=; expires=2022-09-23T03:12:21.000Z; path=${option.path};`;
+export const deleteCookie = function (name, option = { path: '/' }) {
+  document.cookie = `${name}=; expires=2000-01-01T01:00:00.000Z; path=${option.path};`;
 };
