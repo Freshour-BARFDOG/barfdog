cb6e7a16a7b40a3c148775a5e3ea5e227b6e2bbf
sbkim <victory289@naver.com>
2022-11-18 15:18:21 +0900

    Add page > 1:1 문의 상세보기 (* 추가작업 필요)
    # Additional tasks
      - Post file API
      - Post form data API

--- src/components/mypage/inquiry/InquiryItem.jsx
+++ src/components/mypage/inquiry/InquiryItem.jsx
@@ -5,28 +5,30 @@ import Link from 'next/link';
 import transformDate from "/util/func/transformDate";
 
 
+export const inquiryStatusIcon = {
+  [QuestionAnswerStatusType.UNANSWERED]: (
+    <i className={`${s.icon} ${s.UNANSWERED}`}>답변대기</i>
+  ),
+  [QuestionAnswerStatusType.ANSWERED]: (
+    <i className={`${s.icon} ${s.ANSWERED}`}>답변완료</i>
+  ),
+  [QuestionAnswerStatusType.MULTIPLE_ANSWERED]: (
+    <>
+      <i className={`${s.icon} ${s.ANSWERED}`}>답변완료</i>
+      <i className={`${s.icon} ${s.MULTIPLE_ANSWERED}`}>추가답변</i>
+    </>
+  ),
+};
+
+
 export const InquiryItem = ({ data }) => {
   const {id, status, title, createdDate } = data;
-  const statusIcon = {
-    [QuestionAnswerStatusType.UNANSWERED]: (
-      <i className={`${s.icon} ${s.UNANSWERED}`}>답변대기</i>
-    ),
-    [QuestionAnswerStatusType.ANSWERED]: (
-      <i className={`${s.icon} ${s.ANSWERED}`}>답변완료</i>
-    ),
-    [QuestionAnswerStatusType.MULTIPLE_ANSWERED]: (
-      <>
-        <i className={`${s.icon} ${s.ANSWERED}`}>답변완료</i>
-        <i className={`${s.icon} ${s.MULTIPLE_ANSWERED}`}>추가답변</i>
-      </>
-    ),
-  };
 
   return (
     <Link href={`/mypage/inquiry/${id}`} passHref>
       <a>
         <li className={s.tr}>
-          <span className={s.status}>{statusIcon[status]}</span>
+          <span className={s.status}>{inquiryStatusIcon[status]}</span>
             <div className={s.title}><span>{title}</span></div>
           <span className={s.date}>{transformDate(createdDate,'time', {seperator:'.'})}</span>
         </li>
--- src/pages/mypage/inquiry/[id].jsx
+++ src/pages/mypage/inquiry/[id].jsx
@@ -1,45 +1,227 @@
+import React, { useState } from 'react';
 import MypageWrapper from '/src/components/mypage/MypageWrapper';
 import Wrapper from '/src/components/common/Wrapper';
 import Layout from '/src/components/common/Layout';
-import MetaTitle from "/src/components/atoms/MetaTitle";
-import React from "react";
+import MetaTitle from '/src/components/atoms/MetaTitle';
+import s from './inquiry.module.scss';
+import Spinner from '/src/components/atoms/Spinner';
+import { inquiryStatusIcon } from '/src/components/mypage/inquiry/InquiryItem';
+import { QuestionAnswerStatusType } from '/store/TYPE/questionAnswerStatusType';
+import {getDataSSR, postObjData} from '../../api/reqData';
+import transformDate from '/util/func/transformDate';
+import Link from "next/link";
+import {useModalContext} from "/store/modal-context";
+import Modal_confirm from "/src/components/modal/Modal_confirm";
 
-export default function InquiryArticlePage() {
+export default function InquiryArticlePage({ data }) {
+  // console.log(data);
+  
+  const mct = useModalContext();
+  const hasAlert = mct.hasAlert;
+  const [info, setInfo] = useState(data);
+  const [isLoading, setIsLoading] = useState({});
+  const [confirmModal, setConfirmModal] = useState( false );
+  
+  
+  const onStartConfirm = ()=>{
+    setConfirmModal(true);
+  }
+  
+  const onDeleteItem = async (confirm) => {
+    setConfirmModal(false);
+    if(!confirm) return ;
+    
+    try {
+      setIsLoading((prevState) => ({
+        ...prevState,
+        delete: true,
+      }));
+  
+  
+      const body = {
+        // ! 현재 API 작업 중
+        // ! 현재 API 작업 중
+        // ! 현재 API 작업 중
+        // ! 현재 API 작업 중
+      };
+  
+      
+      const apiUrl = '/api/____/____';
+      const res = await postObjData(apiUrl, body);
+    
+      if (res.isDone) {
+        setSubmitted(true);
+        mct.alertShow('성공적으로 1:1 문의등록 삭제되었습니다.', onSuccessCallback);
+      } else {
+        mct.alertShow(res.error, '\n내부 통신장애입니다. 잠시 후 다시 시도해주세요.');
+      }
+    
+    } catch (err) {
+      mct.alertShow('API통신 오류가 발생했습니다. 서버관리자에게 문의하세요.', onFailPostApiCallback);
+    
+      console.error('API통신 오류 : ', err);
+    } finally {
+      setIsLoading((prevState) => ({
+        ...prevState,
+        delete: false,
+      }));
+    }
+  };
+  
+  const onSuccessCallback = ()=>{
+    window.location.href='/mypage/inquiry'
+  }
+  
+  const onFailPostApiCallback = ()=>{
+    window.location.href='/mypage/inquiry'
+  }
+
+ 
   return (
     <>
       <MetaTitle title="마이페이지 1:1 문의 확인" />
       <Layout>
         <Wrapper>
           <MypageWrapper>
-            마이페이지 1:1문의 확인하기
+            <h1>1:1 문의</h1>
+            <section className={s['body-section']}>
+              <div className={s['info-row']}>
+                <div className={s['info-row-title']}>답변</div>
+                <div className={s['info-row-cont']}>
+                  <span className={s['status-box']}>{inquiryStatusIcon[info.status]}</span>
+                </div>
+              </div>
+              <div className={s['info-row']}>
+                <div className={s['info-row-title']}>제목</div>
+                <div className={s['info-row-cont']}>{info.title}</div>
+              </div>
+              <div className={s['info-row']}>
+                <div className={s['info-row-title']}>등록일</div>
+                <div className={s['info-row-cont']}>
+                  {transformDate(info.createdDate, 'time', { seperator: '.' })}
+                </div>
+              </div>
+              <div className={`${s['info-row']} ${s['contents']}`}>
+                <div className={s['info-row-title']}><span>문의내용</span></div>
+                <div className={s['info-row-cont']}>{info.contents}</div>
+              </div>
+            </section>
+            {info.adminAnswer.length > 0 &&
+              info.adminAnswer.map((answer) => (
+                <article className={`${s['answer-section']}`}>
+                  <div className={s['info-row']}>
+                    <div className={s['info-row-title']}>제목</div>
+                    <div className={s['info-row-cont']}>{answer.title}</div>
+                  </div>
+                  <div className={s['info-row']}>
+                    <div className={s['info-row-title']}>등록일</div>
+                    <div className={s['info-row-cont']}>{answer.createdDate}</div>
+                  </div>
+                  <div className={`${s['info-row']} ${s['contents']}`}>
+                    <div className={s['info-row-title']}>문의답변</div>
+                    <div className={s['info-row-cont']}>{answer.contents}</div>
+                  </div>
+                </article>
+              ))}
+            <section className={`${s['btn-section']}`}>
+              <button
+                type={'button'}
+                className={`custom_btn line confirm_m ${s.delete}`}
+                onClick={onStartConfirm}
+              >
+                {isLoading.delete ? <Spinner /> : '삭제'}
+              </button>
+              <Link href={'/mypage/inquiry'} passHref>
+                <a
+                  type={'button'}
+                  className={`custom_btn solid confirm_m ${s.confirm}`}
+                >
+                  목록보기
+                </a>
+              </Link>
+            </section>
           </MypageWrapper>
         </Wrapper>
       </Layout>
+      {confirmModal && <Modal_confirm
+        text={'작성한 문의글과 답글이 삭제됩니다.\n정말 삭제하시겠습니까?'}
+        isConfirm={onDeleteItem}
+        positionCenter
+      />}
     </>
   );
 }
 
-
-
-
-export async function getServerSideProps ({req, query}) {
-  
+export async function getServerSideProps({ req, query }) {
   let DATA = null;
   const id = Number(query.id);
-  
-  
+  const apiUrl = `/api/____/${id}`;
+  // const res = await getDataSSR(req, getApiUrl); // ! PROD
+  const res = DUMMY_RESPONSE; // ! TEST
+  console.log(res);
+
+  if (res.data && res.status === 200) {
+    const data = res.data._embedded;
+    DATA = {
+      id: data.userInquiryDto.id,
+      status: data.userInquiryDto.status,
+      title: data.userInquiryDto.title,
+      contents: data.userInquiryDto.contents,
+      createdDate: data.userInquiryDto.createdDate,
+      adminAnswer: data.adminAnswerDto.map((item) => ({
+        id: item.id,
+        contents: item.contents,
+        createdDate: item.createdDate,
+      })),
+    };
+  }
+
   // ! TODO: 추후 데이터가 없는 경우도 Redir
-  if(typeof id !== 'number' || !id) {
+  if (typeof id !== 'number' || !id) {
     return {
-      redirect : {
-        destination: '/mypage/inquiry'
-      }
-    }
+      redirect: {
+        destination: '/mypage/inquiry',
+      },
+    };
   }
-  
-  
+
   return {
-    props: {data: DATA}
-  }
-  
+    props: { data: DATA },
+  };
 }
+
+const DUMMY_RESPONSE = {
+  data: {
+    _embedded: {
+      userInquiryDto: {
+        id: 1,
+        status: QuestionAnswerStatusType.MULTIPLE_ANSWERED,
+        title: '문의사항있습니다.',
+        contents:
+          'Lorem ipsum dolor sit amet \nLorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, dolor esse et, explicabo facere hic illo in, nemo omnis quidem repudiandae vitae! Asperiores, delectus ducimus facilis harum porro quisquam voluptate?',
+        createdDate: '2022-11-17T14:10:40'
+      },
+      adminAnswerDto: [
+        {
+          id: 1,
+          contents:
+            '관리자 첫 번째 답변입니다.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, dolor esse et, explicabo facere hic illo in, nemo omnis quidem repudiandae vitae! Asperiores, delectus ducimus facilis harum porro quisquam voluptate?',
+          createdDate: '2022-11-18T14:10:40',
+        },
+        {
+          id: 2,
+          contents:
+            '관리자 추가 답변Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, dolor esse et, explicabo facere hic illo in, nemo omnis quidem repudiandae vitae! Asperiores, delectus ducimus facilis harum porro quisquam voluptate?',
+          createdDate: '2022-11-19T14:10:40',
+        },
+        {
+          id: 3,
+          contents:
+            '관리자 추가 답변 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, dolor esse et, explicabo facere hic illo in, nemo omnis quidem repudiandae vitae! Asperiores, delectus ducimus facilis harum porro quisquam voluptate?',
+          createdDate: '2022-11-20T14:10:40',
+        },
+      ],
+    },
+  },
+  status: 200,
+};
