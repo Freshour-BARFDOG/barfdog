1e7b10d82dafa4c02fd4bb6dba75be016eb59fe7
sbkim <victory289@naver.com>
2022-11-22 19:41:15 +0900

    Bug fix
    - Change inconsistent method names

--- util/func/getDtataSSR_inquiryAuthorType.js
+++ util/func/getDtataSSR_inquiryAuthorType.js
@@ -3,10 +3,12 @@ import {inquiryAuthorType} from '../../store/TYPE/inquiry/inquiryAuthorType';
 
 export const getDtataSSR_inquiryAuthorType = async (req, id) => {
   let AUTHOR_TYPE = null;
-  const apiUrl = `/api/questions/type${id}`;
+  const apiUrl = `/api/questions/type/${id}`;
   const res = await getDataSSR( req, apiUrl );
   if ( res.data && res.status === 200 ) {
-    AUTHOR_TYPE = inquiryAuthorType[res.data.type.uppercase()];
+    const type = res.data.type.toUpperCase();
+    AUTHOR_TYPE = inquiryAuthorType[type];
+    
   }
   return AUTHOR_TYPE;
 };
