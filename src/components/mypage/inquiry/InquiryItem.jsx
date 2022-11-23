599293c77cd331089f03f6285d3e3833b0b3611b
sbkim <victory289@naver.com>
2022-11-23 13:14:17 +0900

    Add category name to inquiry title

--- src/components/mypage/inquiry/InquiryItem.jsx
+++ src/components/mypage/inquiry/InquiryItem.jsx
@@ -3,17 +3,19 @@ import s from '/src/pages/mypage/inquiry/inquiry.module.scss';
 import Link from 'next/link';
 import transformDate from "/util/func/transformDate";
 import {inquiryStatusIcon} from "../../../../store/TYPE/inquiry/InquiryStatusIcon";
+import {inquiryCategoryType} from "../../../../store/TYPE/inquiry/inquiryCategoryType";
 
 
 export const InquiryItem = ({ data }) => {
-  const {id, answerStatus, title, createdDate } = data;
+  const {id, answerStatus, title, createdDate, category } = data;
 
+  
   return (
     <Link href={`/mypage/inquiry/${id}`} passHref>
       <a>
         <li className={s.tr}>
           <span className={s.status}>{inquiryStatusIcon[answerStatus]}</span>
-            <div className={s.title}><span>{title}</span></div>
+            <div className={s.title}><span>[{inquiryCategoryType.KOR[category]}] {title}</span></div>
           <span className={s.date}>{transformDate(createdDate,'time', {seperator:'.'})}</span>
         </li>
       </a>
