1c629e9e7ffa02e139dd0a652aaaf5a13f600cf2
sbkim <victory289@naver.com>
2022-12-05 19:07:02 +0900

    Fix bug: Modify the content of the search results for those who do not have subscription information

--- src/pages/mypage/orderHistory/index.jsx
+++ src/pages/mypage/orderHistory/index.jsx
@@ -30,6 +30,7 @@ export default function OrderHistoryPage() {
   const [itemList, setItemList] = useState([]);
   const [itemType, setItemType] = useState(productType.SUBSCRIBE);
 
+  console.log(itemList);
   useEffect(() => {
     // set Scroll Position: 페이지 최상단으로 scroll 위치 초기화 보완
     if (window && typeof window !== 'undefined') {
@@ -42,14 +43,24 @@ export default function OrderHistoryPage() {
     console.log(res);
     // res = activeMenu === 'left' ? DUMMY_SUBSCRIBE_DELVIERY_RESPONSE : DUMMY_GENERAL_DELVIERY_RESPONSE; // ! TEST
     // res = activeMenu === 'left' ? res : DUMMY_GENERAL_DELVIERY_RESPONSE; // ! TEST
-    const pageData = res.data.page;
-    let subscribeItemList = [], generalItemList = [];
-    if(res.data._embedded){
+    
+    let newPageInfo = {
+      totalPages: 0,
+      size: 0,
+      totalItems: 0,
+      currentPageIndex: 1,
+      newPageNumber: 1,
+      newItemList: [],
+    };
+    
+    if(res?.data?._embedded){
+      
       const tmepItemList =
-        res.data?._embedded[
+        res.data._embedded[
           activeMenu === 'left' ? 'querySubscribeOrdersDtoList' : 'queryGeneralOrdersDtoList'
           ] || [];
-      subscribeItemList =
+      
+      const subscribeItemList =
         activeMenu === 'left' &&
         tmepItemList.map((item) => ({
           recipeDto: {
@@ -68,7 +79,8 @@ export default function OrderHistoryPage() {
           },
           link: item._links.query_subscribeOrder.href, // 구독상세보기 Link
         }));
-      generalItemList =
+      
+      const generalItemList =
         activeMenu === 'right' &&
         tmepItemList.map((item) => ({
           thumbnailUrl: item.thumbnailUrl,
@@ -82,19 +94,20 @@ export default function OrderHistoryPage() {
           itemNameList: item.itemNameList,
           link: item._links.query_order.href,
         }));
+      
+      const pageData = res?.data.page;
+      newPageInfo = {
+        totalPages: pageData.totalPages,
+        size: pageData.size,
+        totalItems: pageData.totalElements,
+        currentPageIndex: pageData.number,
+        newPageNumber: pageData.number + 1,
+        newItemList: activeMenu === 'left' ? subscribeItemList : generalItemList,
+      };
     }
-
-    
-
-    let newPageInfo = {
-      totalPages: pageData.totalPages,
-      size: pageData.size,
-      totalItems: pageData.totalElements,
-      currentPageIndex: pageData.number,
-      newPageNumber: pageData.number + 1,
-      newItemList: activeMenu === 'left' ? subscribeItemList : generalItemList,
-    };
+  
     setItemType(activeMenu === 'left' ? productType.SUBSCRIBE : productType.GENERAL);
+    
     return newPageInfo;
   };
 
@@ -122,7 +135,7 @@ export default function OrderHistoryPage() {
                   <EmptyContMessage
                     message={'구독 중인 상품내역이 없습니다.'}
                     options={{ button: { url: '/surveyGuide', label: '정기구독 시작하기' } }}
-                  />
+                  ></EmptyContMessage>
                 ) : (
                   itemType === productType.SUBSCRIBE && <SubscribeItems itemList={itemList} />
                 )}
@@ -161,193 +174,199 @@ export default function OrderHistoryPage() {
 
 
 
-const DUMMY_SUBSCRIBE_DELVIERY_RESPONSE = {
-  data: {
-    _embedded: {
-      querySubscribeOrdersDtoList: [
-        {
-          recipeDto: {
-            thumbnailUrl: 'https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
-            recipeName: '스타트',
-          },
-          subscribeOrderDto: {
-            orderId: 362,
-            subscribeId: 311,
-            orderDate: '2022-07-22T09:56:09.043',
-            dogName: '김바프',
-            subscribeCount: 9,
-            merchantUid: 'merchant_uid8',
-            paymentPrice: 120000,
-            orderStatus: 'DELIVERY_READY',
-          },
-          _links: {
-            query_subscribeOrder: {
-              href: 'http://localhost:8080/api/orders/319/subscribe',
-            },
-            query_subscribe: {
-              href: 'http://localhost:8080/api/subscribes/311',
-            },
-          },
-        },
-        {
-          recipeDto: {
-            thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-            recipeName: '스타트',
-          },
-          subscribeOrderDto: {
-            orderId: 309,
-            subscribeId: 301,
-            orderDate: '2022-07-22T09:56:09',
-            dogName: '김바프',
-            subscribeCount: 8,
-            merchantUid: 'merchant_uid7',
-            paymentPrice: 170000,
-            orderStatus: 'DELIVERY_READY',
-          },
-          _links: {
-            query_subscribeOrder: {
-              href: 'http://localhost:8080/api/orders/309/subscribe',
-            },
-            query_subscribe: {
-              href: 'http://localhost:8080/api/subscribes/301',
-            },
-          },
-        },
-        {
-          recipeDto: {
-            thumbnailUrl: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-            recipeName: '스타트',
-          },
-          subscribeOrderDto: {
-            orderId: 299,
-            subscribeId: 291,
-            orderDate: '2022-07-22T09:56:08.964',
-            dogName: '김바프',
-            subscribeCount: 7,
-            merchantUid: 'merchant_uid6',
-            paymentPrice: 58000,
-            orderStatus: 'DELIVERY_READY',
-          },
-          _links: {
-            query_subscribeOrder: {
-              href: 'http://localhost:8080/api/orders/299/subscribe',
-            },
-            query_subscribe: {
-              href: 'http://localhost:8080/api/subscribes/291',
-            },
-          },
-        }
-      ],
-    },
-    _links: {
-      first: {
-        href: 'http://localhost:8080/api/orders/subscribe?page=0&size=5',
-      },
-      prev: {
-        href: 'http://localhost:8080/api/orders/subscribe?page=0&size=5',
-      },
-      self: {
-        href: 'http://localhost:8080/api/orders/subscribe?page=1&size=5',
-      },
-      next: {
-        href: 'http://localhost:8080/api/orders/subscribe?page=2&size=5',
-      },
-      last: {
-        href: 'http://localhost:8080/api/orders/subscribe?page=2&size=5',
-      },
-      profile: {
-        href: '/docs/index.html#resources-query-subscribeOrders',
-      },
-    },
-    page: {
-      size: 5,
-      totalElements: 13,
-      totalPages: 3,
-      number: 1,
-    },
-  },
-};
+//
+//
+//
+// const DUMMY_SUBSCRIBE_DELVIERY_RESPONSE = {
+//   data: {
+//     _embedded: {
+//       querySubscribeOrdersDtoList: [
+//         {
+//           recipeDto: {
+//             thumbnailUrl: 'https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+//             recipeName: '스타트',
+//           },
+//           subscribeOrderDto: {
+//             orderId: 362,
+//             subscribeId: 311,
+//             orderDate: '2022-07-22T09:56:09.043',
+//             dogName: '김바프',
+//             subscribeCount: 9,
+//             merchantUid: 'merchant_uid8',
+//             paymentPrice: 120000,
+//             orderStatus: 'DELIVERY_READY',
+//           },
+//           _links: {
+//             query_subscribeOrder: {
+//               href: 'http://localhost:8080/api/orders/319/subscribe',
+//             },
+//             query_subscribe: {
+//               href: 'http://localhost:8080/api/subscribes/311',
+//             },
+//           },
+//         },
+//         {
+//           recipeDto: {
+//             thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+//             recipeName: '스타트',
+//           },
+//           subscribeOrderDto: {
+//             orderId: 309,
+//             subscribeId: 301,
+//             orderDate: '2022-07-22T09:56:09',
+//             dogName: '김바프',
+//             subscribeCount: 8,
+//             merchantUid: 'merchant_uid7',
+//             paymentPrice: 170000,
+//             orderStatus: 'DELIVERY_READY',
+//           },
+//           _links: {
+//             query_subscribeOrder: {
+//               href: 'http://localhost:8080/api/orders/309/subscribe',
+//             },
+//             query_subscribe: {
+//               href: 'http://localhost:8080/api/subscribes/301',
+//             },
+//           },
+//         },
+//         {
+//           recipeDto: {
+//             thumbnailUrl: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+//             recipeName: '스타트',
+//           },
+//           subscribeOrderDto: {
+//             orderId: 299,
+//             subscribeId: 291,
+//             orderDate: '2022-07-22T09:56:08.964',
+//             dogName: '김바프',
+//             subscribeCount: 7,
+//             merchantUid: 'merchant_uid6',
+//             paymentPrice: 58000,
+//             orderStatus: 'DELIVERY_READY',
+//           },
+//           _links: {
+//             query_subscribeOrder: {
+//               href: 'http://localhost:8080/api/orders/299/subscribe',
+//             },
+//             query_subscribe: {
+//               href: 'http://localhost:8080/api/subscribes/291',
+//             },
+//           },
+//         }
+//       ],
+//     },
+//     _links: {
+//       first: {
+//         href: 'http://localhost:8080/api/orders/subscribe?page=0&size=5',
+//       },
+//       prev: {
+//         href: 'http://localhost:8080/api/orders/subscribe?page=0&size=5',
+//       },
+//       self: {
+//         href: 'http://localhost:8080/api/orders/subscribe?page=1&size=5',
+//       },
+//       next: {
+//         href: 'http://localhost:8080/api/orders/subscribe?page=2&size=5',
+//       },
+//       last: {
+//         href: 'http://localhost:8080/api/orders/subscribe?page=2&size=5',
+//       },
+//       profile: {
+//         href: '/docs/index.html#resources-query-subscribeOrders',
+//       },
+//     },
+//     page: {
+//       size: 5,
+//       totalElements: 13,
+//       totalPages: 3,
+//       number: 1,
+//     },
+//   },
+// };
+//
+//
 
-const DUMMY_GENERAL_DELVIERY_RESPONSE = {
-  data: {
-    _embedded: {
-      queryGeneralOrdersDtoList: [
-        {
-          thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-          orderDate: '2022-08-22T09:56:08.964',
-          orderDto: {
-            id: 362,
-            merchantUid: 'merchant_uid8',
-            paymentPrice: 110000,
-            orderStatus: 'BEFORE_PAYMENT',
-            orderDate: '2022-08-22T09:56:08.964',
-          },
-          itemNameList: ['굿즈 상품1', '굿즈 상품2'],
-          _links: {
-            query_order: {
-              href: 'http://localhost:8080/api/orders/696/general',
-            },
-          },
-        },
-        {
-          thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-          orderDto: {
-            id: 675,
-            merchantUid: 'merchant_uid7',
-            paymentPrice: 170000,
-            orderStatus: 'BEFORE_PAYMENT',
-            orderDate: '2022-08-12T09:56:08.964',
-          },
-          itemNameList: ['굿즈 상품1', '굿즈 상품2'],
-          _links: {
-            query_order: {
-              href: 'http://localhost:8080/api/orders/675/general',
-            },
-          },
-        },
-        {
-          thumbnailUrl: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-          orderDto: {
-            id: 654,
-            merchantUid: 'merchant_uid6',
-            paymentPrice: 78000,
-            orderStatus: 'BEFORE_PAYMENT',
-            orderDate: '2022-07-22T09:56:08.964',
-          },
-          itemNameList: ['굿즈 상품1', '굿즈 상품2'],
-          _links: {
-            query_order: {
-              href: 'http://localhost:8080/api/orders/654/general',
-            },
-          },
-        }
-      ],
-    },
-    _links: {
-      first: {
-        href: 'http://localhost:8080/api/orders/general?page=0&size=5',
-      },
-      prev: {
-        href: 'http://localhost:8080/api/orders/general?page=0&size=5',
-      },
-      self: {
-        href: 'http://localhost:8080/api/orders/general?page=1&size=5',
-      },
-      next: {
-        href: 'http://localhost:8080/api/orders/general?page=2&size=5',
-      },
-      last: {
-        href: 'http://localhost:8080/api/orders/general?page=2&size=5',
-      },
-      profile: {
-        href: '/docs/index.html#resources-query-generalOrders',
-      },
-    },
-    page: {
-      size: 5,
-      totalElements: 13,
-      totalPages: 3,
-      number: 1,
-    },
-  },
-};
+//
+// const DUMMY_GENERAL_DELVIERY_RESPONSE = {
+//   data: {
+//     _embedded: {
+//       queryGeneralOrdersDtoList: [
+//         {
+//           thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+//           orderDate: '2022-08-22T09:56:08.964',
+//           orderDto: {
+//             id: 362,
+//             merchantUid: 'merchant_uid8',
+//             paymentPrice: 110000,
+//             orderStatus: 'BEFORE_PAYMENT',
+//             orderDate: '2022-08-22T09:56:08.964',
+//           },
+//           itemNameList: ['굿즈 상품1', '굿즈 상품2'],
+//           _links: {
+//             query_order: {
+//               href: 'http://localhost:8080/api/orders/696/general',
+//             },
+//           },
+//         },
+//         {
+//           thumbnailUrl: 'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+//           orderDto: {
+//             id: 675,
+//             merchantUid: 'merchant_uid7',
+//             paymentPrice: 170000,
+//             orderStatus: 'BEFORE_PAYMENT',
+//             orderDate: '2022-08-12T09:56:08.964',
+//           },
+//           itemNameList: ['굿즈 상품1', '굿즈 상품2'],
+//           _links: {
+//             query_order: {
+//               href: 'http://localhost:8080/api/orders/675/general',
+//             },
+//           },
+//         },
+//         {
+//           thumbnailUrl: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+//           orderDto: {
+//             id: 654,
+//             merchantUid: 'merchant_uid6',
+//             paymentPrice: 78000,
+//             orderStatus: 'BEFORE_PAYMENT',
+//             orderDate: '2022-07-22T09:56:08.964',
+//           },
+//           itemNameList: ['굿즈 상품1', '굿즈 상품2'],
+//           _links: {
+//             query_order: {
+//               href: 'http://localhost:8080/api/orders/654/general',
+//             },
+//           },
+//         }
+//       ],
+//     },
+//     _links: {
+//       first: {
+//         href: 'http://localhost:8080/api/orders/general?page=0&size=5',
+//       },
+//       prev: {
+//         href: 'http://localhost:8080/api/orders/general?page=0&size=5',
+//       },
+//       self: {
+//         href: 'http://localhost:8080/api/orders/general?page=1&size=5',
+//       },
+//       next: {
+//         href: 'http://localhost:8080/api/orders/general?page=2&size=5',
+//       },
+//       last: {
+//         href: 'http://localhost:8080/api/orders/general?page=2&size=5',
+//       },
+//       profile: {
+//         href: '/docs/index.html#resources-query-generalOrders',
+//       },
+//     },
+//     page: {
+//       size: 5,
+//       totalElements: 13,
+//       totalPages: 3,
+//       number: 1,
+//     },
+//   },
+// };
