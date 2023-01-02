267dbb775b8d6d6a1bc069d130f7f9b07950e8ad
sbkim <victory289@naver.com>
2023-01-02 15:46:55 +0900

    Refactoring & Fix Bug on `Search Term UI`(조회기간 검색 UI)
    * Refactoring 1
     : 중복되는 코드 줄임:  map을 활용하여, Button Component로 변환
    
    * Refactoring 2
     : 조회기간과 일치하는 조회기간버튼 탐색방식 변경
     ( 기존: useRef => 변경: JSX 내에 조건문 )
     - 이유: 위 component 방식으로 변경한 뒤에는 기존 useRef방식으로 조건에 맞는 UI변경을 할 수 없음
     - 부연설명
      1. useRef: current.children 사용 (Component방식의 Button찾을 수 없음)
      2. SearchTermButton의 ClassName에 조건문 적용
    
    * Fix Bug
     : 조회기간의 날짜 직접입력 시, 조회기간버튼과 일치하는 기간차이일 경우 해당 버튼 활성화 UI 적용

--- src/components/admin/form/searchBar/SearchTerm.jsx
+++ src/components/admin/form/searchBar/SearchTerm.jsx
@@ -1,63 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useRef, useCallback} from 'react';
 import s from './searchBar.module.scss';
-import siblings from '@util/func/siblings';
-import ToolTip from '@src/components/atoms/Tooltip';
-import { transformToday } from '/util/func/transformDate';
+import {transformToday} from '/util/func/transformDate';
 import {getDiffDate, getDiffDateNumber} from '/util/func/getDiffDate';
 import {global_searchDateType} from "/store/TYPE/searchDateType";
 
 
-//내부에서 from 과 to값을 비교해서, 그차이만큼의 값이랑 button의 inner value의 값이 동일하면 active가 되도록만든다. 
-
-
-const SearchTerm = ({ searchValue, setSearchValue, title, tooltip }) => {
+const SearchTerm = ({searchValue, setSearchValue, title, tooltip}) => {
+  const {from, to} = searchValue;
   const optionalRef = useRef();
-
-  useEffect(() => {
-    const diffDate = getDiffDateNumber(searchValue.from, searchValue.to);
-    initializeOptionalButtons(diffDate);
-  }, [searchValue]);
-
-  const initializeOptionalButtons = (diffDateNum) => {
-    const optionalButtons = Array.from(optionalRef.current.children);
-    optionalButtons.forEach((t) => {
-      const dateValue = t.dataset.value;
-      if(Number(dateValue) === diffDateNum){
-        t.classList.add(s.active)
-      } else {
-        t.classList.remove(s.active)
-      }
-    });
-  };
-
-  const onOptionalTermHandler = (e) => {
+  const diffDate = getDiffDateNumber( from, to );
+  
+  const onChangeTerm = useCallback( (e) => {
     const target = e.currentTarget;
-    const value = target.dataset.value;
+    const diffDate = Number( target.dataset.value );
+    const diffDateFromPast = diffDate * -1;
+    
     const to = transformToday();
-    const from = value === 'all' ? global_searchDateType.lastTime : getDiffDate(value);
-    setSearchValue((prevState) => ({
+    const from = diffDate === 'all' ? global_searchDateType.oldestDate : getDiffDate( diffDateFromPast );
+    
+    setSearchValue( (prevState) => ({
       ...prevState,
       from,
       to,
-    }));
-    // target.classList.add(s.active);
-    // siblings(target).forEach((t) => t.classList.remove(s.active));
-  };
-
-  const onDriectTermHandler = (e) => {
+    }) );
+  }, [] );
+  
+  const onDriectTermHandler = useCallback( (e) => {
     const dateInput = e.currentTarget;
-    const { id, value} = dateInput;
-
-    setSearchValue((prevState) => ({
+    const {id, value} = dateInput;
+    
+    setSearchValue( (prevState) => ({
       ...prevState,
       [id]: value
-    }));
-
-    const optionalCategoryList = Array.from(optionalRef.current.children);
-    optionalCategoryList.forEach((t) => t.classList.remove(s.active));
-  };
-  // searcj Value의 from값과
-
+    }) );
+  }, [] );
+  
+  
   return (
     <>
       <div className={s['search-row']}>
@@ -66,56 +44,15 @@ const SearchTerm = ({ searchValue, setSearchValue, title, tooltip }) => {
           {tooltip && <span className={s['tooltip-wrap']}>{tooltip}</span>}
         </h4>
         <div className={`${s['inp-wrap']} ${s['term']}`}>
-          <div ref={optionalRef}>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={0}
-              className={`admin_btn line basic_l`}
+          <div >
+            {global_searchDateType.BUTTONS.map( (btn, i) => <button
+              key={`SearchTermButton-${i}`}
+              onClick={onChangeTerm}
+              data-value={btn.diffDate}
+              className={`admin_btn line basic_l ${Math.abs( diffDate ) === btn.diffDate ? s.active : ''}`}
             >
-              오늘
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={-3}
-              className="admin_btn line basic_l"
-            >
-              3일
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={-7}
-              className="admin_btn line basic_l"
-            >
-              7일
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={-30}
-              className="admin_btn line basic_l"
-            >
-              30일
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={-60}
-              className="admin_btn line basic_l"
-            >
-              60일
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={-120}
-              className="admin_btn line basic_l"
-            >
-              120일
-            </button>
-            <button
-              onClick={onOptionalTermHandler}
-              data-value={global_searchDateType.lastTimeDiffDate}
-              className="admin_btn line basic_l"
-            >
-              전체
-            </button>
+              {btn.label}
+            </button> )}
           </div>
         </div>
         <div className={`${s['inp-wrap']} ${s['term']}`}>
@@ -143,5 +80,3 @@ const SearchTerm = ({ searchValue, setSearchValue, title, tooltip }) => {
 };
 
 export default SearchTerm;
-
-
--- store/TYPE/searchDateType.js
+++ store/TYPE/searchDateType.js
@@ -1,9 +1,43 @@
-import {transformToday} from "/util/func/transformDate";
-import {getDiffDateNumber} from "/util/func/getDiffDate";
+import { transformToday } from '/util/func/transformDate';
+import { getDiffDateNumber } from '/util/func/getDiffDate';
+
+const oldestDate = '1900-01-01';
+const maxDiffDate = getDiffDateNumber(transformToday(), oldestDate);
+
+const searchDateList = [
+  {
+    diffDate: 0,
+    label: '오늘',
+  },
+  {
+    diffDate: 3,
+    label: '3일',
+  },
+  {
+    diffDate: 7,
+    label: '7일',
+  },
+  {
+    diffDate: 30,
+    label: '30일',
+  },
+  {
+    diffDate: 60,
+    label: '60일',
+  },
+  {
+    diffDate: 120,
+    label: '120일',
+  },
+  {
+    diffDate: maxDiffDate,
+    label: '전체',
+  },
+];
 
 export const global_searchDateType = {
-  lastTime: '1900-01-01', // '가장 오래된 데이터 검색일자'
-  lastTimeDiffDate: getDiffDateNumber( '1900-01-01', transformToday()), // '사이트 검색 중 가장 오래된 순 검색 시 사용'
-  firstTime: transformToday(), // 오늘
-  test: this,
-}
+  firstTime: transformToday(),
+  oldestDate: oldestDate,
+  maxDiffDate: maxDiffDate, // '사이트 검색 중 가장 오래된 순 검색 시 사용'
+  BUTTONS: searchDateList,
+};
