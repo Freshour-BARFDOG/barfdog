90a5ffb3f4ac5d391c58c72275d3fde3be98c7a2
sbkim <victory289@naver.com>
2023-01-02 15:30:05 +0900

    Delete unnecessary Code on web.js
    - web.js: 모든 js 파일 중에서 가장 먼저 실행되는 Serverside Code
    
    - 개발초기 단계에서 CORS 해결을 위해 web.js Request Handler에 CORS 옵션을 추가하였음
    
    - 추후 확인한 결과 Response Header > Access-Control-Allow 옵션을 사용하지 않더라도, ADMIN 내에서 CORS 오류가 발생하지 않음
    
    - 따라서 해당 코드는 의미가 없으므로, 사용하지 않음.

--- web.js
+++ web.js
@@ -1,8 +1,6 @@
 const { createServer: http } = require('http');
-const { createServer: https } = require('https');
 const express = require('express');
 const server = express();
-const fs = require('fs');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
@@ -17,17 +15,16 @@ const ports = {
 nextApp
   .prepare()
   .then(() => {
-    
     server.all('*', (req, res) => {
-      const barfdogApiUrl = dev ? process.env.NEXT_PUBLIC_API_URL_DEV : process.env.NEXT_PUBLIC_API_URL_PRODUCT
-      console.log(barfdogApiUrl);
-      res.setHeader("Access-Control-Allow-Credentials", true); // 쿠키 주고받기 허용
-      res.setHeader("Access-Control-Allow-Origin", barfdogApiUrl); // CORS 허용
-      res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT"); // CORS 허용
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-      )
+      // const barfdogApiUrl = dev ? process.env.NEXT_PUBLIC_API_URL_DEV : process.env.NEXT_PUBLIC_API_URL_PRODUCT
+      // console.log('BARFDOG API URL: ',barfdogApiUrl);
+      // res.setHeader("Access-Control-Allow-Credentials", true); // 쿠키 주고받기 허용
+      // res.setHeader("Access-Control-Allow-Origin", barfdogApiUrl); // CORS 허용
+      // res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT"); // CORS 허용
+      // res.setHeader(
+      //   'Access-Control-Allow-Headers',
+      //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      // )
       return handle(req, res);
     });
 
@@ -37,6 +34,6 @@ nextApp
     });
   })
   .catch((err) => {
-    console.error('ERROR:::', err.stack);
+    console.error('WEB.JS > ERROR:', err.stack);
     process.exit(1);
   });
