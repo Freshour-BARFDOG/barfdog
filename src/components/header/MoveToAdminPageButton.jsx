288645b3e7d551de8c59516feb526a5b6fae669d
sbkim <victory289@naver.com>
2023-01-04 17:41:39 +0900

    UserPage GNB 내에 `관리자페이지` 이동 버튼 추가
    - 조건: 관리자 권한

--- src/components/header/MoveToAdminPageButton.jsx
+++ src/components/header/MoveToAdminPageButton.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {URLPathOnLocalStorageClass} from "../../class/URLPathClass";
 
-export const MoveToAdminPageButton = () =>{
-  return  <li><a href={"/bf-admin/dashboard"} style={{fontSize: "14px"}}>관리자페이지</a></li>;
+
+export const MoveToAdminPageButton = () => {
+  const [adminPath, setAdminPath] = useState( "" );
+  useEffect( () => {
+    const ADMIN_INDEX_PATH = "/bf-admin/dashboard";
+    const p = new URLPathOnLocalStorageClass();
+    const path = p.lastVisitedAdminPath || ADMIN_INDEX_PATH;
+    setAdminPath( path );
+  }, [] );
+  
+  return <li><a href={adminPath} style={{fontSize: "14px"}}>관리자페이지</a></li>;
 }
