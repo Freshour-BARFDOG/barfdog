a0ab7cd339821c3982df5b15ea06319eb5349339
sbkim <victory289@naver.com>
2023-02-13 10:34:22 +0900

    아임포트 REST API KEY, RES API SECRET > env 파일로 추출

--- src/pages/api/iamportSubscribe.js
+++ src/pages/api/iamportSubscribe.js
@@ -22,8 +22,8 @@ export default async function handler(req, res){
         method: "post", // POST method
         headers: { "Content-Type": "application/json" }, // "Content-Type": "application/json"
         data: {
-          imp_key: "8722322371707106", // REST API 키
-          imp_secret: "eb961888a719002923f107e1024bb45d177b8d92279ad7843a35be08c107c4ab01033597b2c21968" // REST API Secret
+          imp_key: process.env.NEXT_PUBLIC_IAMPORT_REST_API_KEY, // REST API 키
+          imp_secret: process.env.NEXT_PUBLIC_IAMPORT_REST_API_SECRET,  // REST API Secret
         }
       });
       
