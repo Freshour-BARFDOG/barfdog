22b1e419e8934bb12e35200585fe01b0ffece60a
sbkim <victory289@naver.com>
2023-02-20 19:22:43 +0900

    #1 - 관리자에서 설정한 plan discount 정보를 담은 subscribeInfo hook 리팩토링  - 해당 hook을 사용 시 확장성을 고려하여 field 별도 구분함.  - 초기값을 별도로 설정하였으나, 그것 또한 오류를 야기할 수 있으므로, null값으로 할당함

--- util/hook/useSubscribePlanInfo.ts
+++ util/hook/useSubscribePlanInfo.ts
@@ -2,20 +2,16 @@ import {useEffect, useState} from "react";
 import {getData} from "@src/pages/api/reqData";
 import {subscribePlanType} from '@store/TYPE/subscribePlanType';
 
-const initialDiscount = {
-  [subscribePlanType.FULL.NAME]: 5,
-  [subscribePlanType.HALF.NAME]: 2,
-  [subscribePlanType.TOPPING.NAME]: 1
-}
-
-const initData = {
-  discountPercent: initialDiscount
-}
 
 export const useSubscribePlanInfo = () => {
-
   
-  const [allData, setAllData] = useState(initData);
+  
+  const [planDiscountPercent, setPlanDiscountPercent] = useState({
+    [subscribePlanType.FULL.NAME]: null,
+    [subscribePlanType.HALF.NAME]: null,
+    [subscribePlanType.TOPPING.NAME]: null
+  });
+  
   const [isLoading, setIsLoading] = useState(false);
   
   
@@ -26,34 +22,26 @@ export const useSubscribePlanInfo = () => {
         const url = `/api/planDiscount`;
         const res = await getData(url);
         // console.log('useSubscribePlanInfo: ',res);
-        let DATA;
-        if(res.data && res.status === 200){
+        if(res.data && res.status === 200) {
           const data = res.data._embedded.planDiscountResponseDtoList[0];
-          DATA = {
+  
+          setPlanDiscountPercent({
             [subscribePlanType.FULL.NAME]: data.full,
             [subscribePlanType.HALF.NAME]: data.half,
             [subscribePlanType.TOPPING.NAME]: data.topping,
-          }
-          setAllData((prev)=>({
-            ...prev,
-            discountPercent: DATA
-            
-          }));
+          });
         }
-        
       } catch (err) {
         console.error(err)
       } finally {
         setIsLoading(false);
       }
-      
     })();
   },[]);
   
-  
   return {
     isLoading,
-    ...allData
+    planDiscountPercent:planDiscountPercent
   }
   
 }
