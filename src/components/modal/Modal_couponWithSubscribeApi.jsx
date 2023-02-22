69d55b1bd8a319caad44b62ef9a541017e6e92c0
sbkim <victory289@naver.com>
2023-02-22 09:36:43 +0900

    #3 - 예약된 정기구독 > 적용된 쿠폰 변경 시, confirm modal 추가 및 layout 변경

--- src/components/modal/Modal_couponWithSubscribeApi.jsx
+++ src/components/modal/Modal_couponWithSubscribeApi.jsx
@@ -10,6 +10,7 @@ import calculateSalePrice, {calculateAndConvertToMinimumSalePrice} from '/util/f
 import transformClearLocalCurrency from '/util/func/transformClearLocalCurrency';
 import {useModalContext} from '/store/modal-context';
 import {postObjData, putObjData} from '/src/pages/api/reqData';
+import Modal_confirm from "./Modal_confirm";
 
 
 export const Modal_couponWithSubscribeApi = ({
@@ -17,6 +18,8 @@ export const Modal_couponWithSubscribeApi = ({
                                                setSubmitted,
                                                event = {hideModal: null},
                                              }) => {
+  
+  const [activeConfirmModal, setActiveConfirmModal] = useState( false );
   // INIT STATE
   const info = {
     subscribeId: data.subscribeId,
@@ -104,20 +107,23 @@ export const Modal_couponWithSubscribeApi = ({
     }
   }
   
-  console.log(data);
   
-  const onCancelUsedCoupon = async () => {
+  const onCancelUsedCoupon = async (confirm) => {
+    setActiveConfirmModal( false ); // 컨펌모달 숨김
+    if ( !confirm ) return; // 쿠폰적용 취소처리 중지
+    
+    
     try {
       setIsLoading( true );
       const body = {
         memberCouponId: info.usedCoupon.usingMemberCouponId,
       };
-      console.log("onCancelUsedCoupon > body", body);
+      console.log( "onCancelUsedCoupon > body", body );
       const url = `/api/subscribes/${info.subscribeId}/coupon/cancel`;
       const res = await putObjData( url, body );
       if ( res.isDone ) {
         setSubmitted( true );
-        mct.alertShow( '사용 중인 쿠폰을 적용취소 처리하였습니다.', onSuccessCallback );
+        mct.alertShow( '사용 중인 쿠폰을 적용 취소 처리하였습니다.', onSuccessCallback );
       } else {
         mct.alertShow( `데이터 전송 실패\n${res.error}` );
       }
@@ -132,6 +138,8 @@ export const Modal_couponWithSubscribeApi = ({
     window.location.reload();
   };
   
+  // console.log( info );
+  
   return (
     <>
       <ModalWrapper
@@ -141,55 +149,83 @@ export const Modal_couponWithSubscribeApi = ({
         positionCenter
       >
         <div className={s.modal}>
-          <section className={s.title_box}>
-            <h3 className={s.title}>사용 가능 쿠폰</h3>
-            {!!info.usedCoupon.usingMemberCouponId && <div className={s.usedCouponBox}>
-              <span className={s.status}>사용중</span>
-              <span className={s.couponName}>{info.usedCoupon.couponName}</span>
-              <span className={s.discount}>{transformLocalCurrency( info.usedCoupon.discountCoupon )}원</span>
-              <button className={'custom_btn basic_m line'} onClick={onCancelUsedCoupon}>취소</button>
-            </div>}
-          </section>
-          <div className={s.content_box}>
-            <div className={`${s.flex_box} ${s.header}`}>
-              <span className={s.coupon_title}>쿠폰명</span>
-              <span>적용가능수량</span>
-              <span>유효기간</span>
-              <span>할인금액</span>
+          {!!info.usedCoupon.usingMemberCouponId && <section className={s["coupon-section"]} data-title={"used-coupon"}>
+            <div className={s.title_box}>
+              <h3 className={s.title}>적용 중인 쿠폰</h3>
             </div>
-          </div>
-          <section className={s.content_box}>
-            {info?.availableCouponList.length > 0 ? (
-              info.availableCouponList
-                .filter((item) => {
-                  // 1. SET Coupon Info
-                  item.couponId = `coupon-${item.memberCouponId}`;
-                  const { salePrice, saleAmount } = calculateSalePrice(
-                    info.originPrice,
-                    item.discountType,
-                    item.discountDegree,
-                  );
-                  item.salePrice = transformClearLocalCurrency(salePrice);
-                  item.discountAmount = transformClearLocalCurrency(saleAmount);
-
-                  // STEP 2. validation
-                  let valid = false;
-                  if (
-                    item.remaining > 0 &&
-                    info.originPrice >= item.availableMinPrice &&
-                    item.discountAmount <= item.availableMaxDiscount
-                  ) {
-                    valid = true;
-                  }
-                  // console.log(item, valid);
-                  return valid && item;
-                })
-                .map((item, index) => (
-                  <label
-                    key={`couopn-list-${item.couponId}`}
-                    className={`${s.flex_box} ${s.coupons}`}
-                    htmlFor={item.couponId}
-                  >
+            <div className={`${s.content_box} ${s['used-coupon']}`}>
+              <div className={`${s.flex_box} ${s.header}`}>
+                <span className={s.coupon_title}>쿠폰명</span>
+                <span>할인금액</span>
+              </div>
+            </div>
+            <div className={`${s.content_box} ${s['used-coupon']}`}>
+              <label className={`${s.flex_box} ${s.coupons}`}>
+                <span className={s.radio}>
+                  <input
+                    type="radio"
+                    name={'used-coupon'}
+                    onChange={null}
+                    checked={true}
+                    disabled={true}
+                  />
+                </span>
+                <span className={s.name}>{info.usedCoupon.couponName}</span>
+                <span className={s.price}>{transformLocalCurrency( info.usedCoupon.discountCoupon )}원</span>
+              </label>
+            </div>
+            <div className={s.btn_box}>
+              <button className={'custom_btn popup line pointColor'} onClick={() => {
+                setActiveConfirmModal( true )
+              }}>적용취소
+              </button>
+            </div>
+          </section>}
+          
+          <section className={s["coupon-section"]} data-title={"available-coupons"}>
+            <section className={s.title_box}>
+              <h3 className={s.title}>사용 가능 쿠폰</h3>
+            </section>
+            <div className={s.content_box}>
+              <div className={`${s.flex_box} ${s.header}`}>
+                <span className={s.coupon_title}>쿠폰명</span>
+                <span>적용가능수량</span>
+                <span>유효기간</span>
+                <span>할인금액</span>
+              </div>
+            </div>
+            <section className={s.content_box}>
+              {info?.availableCouponList.length > 0 ? (
+                info.availableCouponList
+                  .filter((item) => {
+                    // 1. SET Coupon Info
+                    item.couponId = `coupon-${item.memberCouponId}`;
+                    const { salePrice, saleAmount } = calculateSalePrice(
+                      info.originPrice,
+                      item.discountType,
+                      item.discountDegree,
+                    );
+                    item.salePrice = transformClearLocalCurrency(salePrice);
+                    item.discountAmount = transformClearLocalCurrency(saleAmount);
+          
+                    // STEP 2. validation
+                    let valid = false;
+                    if (
+                      item.remaining > 0 &&
+                      info.originPrice >= item.availableMinPrice &&
+                      item.discountAmount <= item.availableMaxDiscount
+                    ) {
+                      valid = true;
+                    }
+                    // console.log(item, valid);
+                    return valid && item;
+                  })
+                  .map((item, index) => (
+                    <label
+                      key={`couopn-list-${item.couponId}`}
+                      className={`${s.flex_box} ${s.coupons}`}
+                      htmlFor={item.couponId}
+                    >
                     <span className={s.radio}>
                       <input
                         id={item.couponId}
@@ -205,63 +241,73 @@ export const Modal_couponWithSubscribeApi = ({
                         }
                       />
                     </span>
-
-                    <span className={s.name}>{`${item.name} (${transformLocalCurrency(
-                      item.discountDegree,
-                    )}${
-                      item.discountType === discountUnitType.FIXED_RATE
-                        ? discountUnitType.KOR.FIXED_RATE
-                        : discountUnitType.KOR.FLAT_RATE
-                    } 할인)`}</span>
-                    <span className={s.count}>{item.remaining}개</span>
-                    <span className={s.date}>{transformDate(item.expiredDate)}</span>
-                    <span className={s.price}>{transformLocalCurrency(item.discountAmount)}원</span>
-                  </label>
-                ))
-            ) : (
-              <EmptyMessage text={'사용가능한 쿠폰이 없습니다.'} />
-            )}
-          </section>
-          <section className={s.calculator}>
-            <div className={s.col_1}>
-              <p>상품 금액</p>
-              <span className={s.text_price}>{transformLocalCurrency( info.originPrice )}원</span>
-            </div>
-            {/*<i className={s.line}></i>*/}
-            <div className={s.col_2}>
-              <p>쿠폰 할인금액</p>
-              <span className={s.text_price}>
+            
+                      <span className={s.name}>{`${item.name} (${transformLocalCurrency(
+                        item.discountDegree,
+                      )}${
+                        item.discountType === discountUnitType.FIXED_RATE
+                          ? discountUnitType.KOR.FIXED_RATE
+                          : discountUnitType.KOR.FLAT_RATE
+                      } 할인)`}</span>
+                      <span className={s.count}>{item.remaining}개</span>
+                      <span className={s.date}>{transformDate(item.expiredDate)}</span>
+                      <span className={s.price}>{transformLocalCurrency(item.discountAmount)}원</span>
+                    </label>
+                  ))
+              ) : (
+                <EmptyMessage text={'사용가능한 쿠폰이 없습니다.'} />
+              )}
+            </section>
+            <section className={s.calculator}>
+              <div className={s.col_1}>
+                <p>상품 금액</p>
+                <span className={s.text_price}>{transformLocalCurrency( info.originPrice )}원</span>
+              </div>
+              {/*<i className={s.line}></i>*/}
+              <div className={s.col_2}>
+                <p>쿠폰 할인금액</p>
+                <span className={s.text_price}>
                 {selectedRadioInfo.discountAmount > 0 && "-"} {transformLocalCurrency( selectedRadioInfo.discountAmount )}원
               </span>
-            </div>
-            {/*<i className={s.line}></i>*/}
-            <div className={s.col_2}>
-              <p>등급 할인금액</p>
-              <span className={s.text_price}>
+              </div>
+              {/*<i className={s.line}></i>*/}
+              <div className={s.col_2}>
+                <p>등급 할인금액</p>
+                <span className={s.text_price}>
                 {info.discountGrade > 0 && "-"} {transformLocalCurrency( info.discountGrade )}원
               </span>
-            </div>
-            {selectedRadioInfo.unappliedDiscountAmount > 0 && <div className={s.col_2}>
-              <p>최소결제 적용금액</p>
-              <span className={s.text_price}>+ {transformLocalCurrency( selectedRadioInfo.unappliedDiscountAmount )}원</span>
-            </div>}
-            <i className={s.vertical_line}></i>
-            <div className={s.col_3}>
-              <h4 className={s.title}>할인 후 금액
-                <span>{transformLocalCurrency( selectedRadioInfo.salePrice )}원</span>
-              </h4>
-            </div>
+              </div>
+              {selectedRadioInfo.unappliedDiscountAmount > 0 && <div className={s.col_2}>
+                <p>최소결제 적용금액</p>
+                <span className={s.text_price}>+ {transformLocalCurrency( selectedRadioInfo.unappliedDiscountAmount )}원</span>
+              </div>}
+              <i className={s.vertical_line}></i>
+              <div className={s.col_3}>
+                <h4 className={s.title}>할인 후 금액
+                  <span>{transformLocalCurrency( selectedRadioInfo.salePrice )}원</span>
+                </h4>
+              </div>
+            </section>
+            <section className={s.btn_box}>
+              <button type={'button'} className={s.cancle_btn} onClick={event.hideModal}>
+                닫기
+              </button>
+              <button type={'button'} className={s.choice_btn} onClick={onApplyingCoupon}>
+                {isLoading.submit ? <Spinner style={{color: '#fff'}}/> : '쿠폰적용'}
+              </button>
+            </section>
           </section>
-          <section className={s.btn_box}>
-            <button type={'button'} className={s.cancle_btn} onClick={event.hideModal}>
-              닫기
-            </button>
-            <button type={'button'} className={s.choice_btn} onClick={onApplyingCoupon}>
-              {isLoading.submit ? <Spinner style={{ color: '#fff' }} /> : '쿠폰적용'}
-            </button>
-          </section>
+          
         </div>
       </ModalWrapper>
+      {activeConfirmModal && (
+        <Modal_confirm
+          theme={'userPage'}
+          isConfirm={onCancelUsedCoupon}
+          positionCenter
+          text={"사용 중인 쿠폰을 적용 취소하시겠습니까?"}
+        />
+      )}
     </>
   );
 };
