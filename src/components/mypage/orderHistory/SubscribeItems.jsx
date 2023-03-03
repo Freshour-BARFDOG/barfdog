f24c20d19e3e4c039297cda317cb0249f060e06c
sbkim <victory289@naver.com>
2023-03-03 11:22:42 +0900

    # 5 - 일반,정기결제 > (주문 전 결제에 대한)`주문포기 API`
    - 여러 가지 이유로 일반결제 / 정기결제 중, 결제포기 처리가 되지 않은 주문에 대해서, 결제 포기 기능을 추가한다.
    
    - 결제포기 API 호출 시, 결제 전 주문에서 사용된 쿠폰, 적립금이 반환된다.

--- src/components/mypage/orderHistory/SubscribeItems.jsx
+++ src/components/mypage/orderHistory/SubscribeItems.jsx
@@ -1,15 +1,48 @@
 import s from '/src/pages/mypage/orderHistory/orderHistory.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import transformDate from '/util/func/transformDate';
 import transformLocalCurrency from '/util/func/transformLocalCurrency';
-import { orderStatus } from '/store/TYPE/orderStatusTYPE';
-
+import {orderStatus} from '/store/TYPE/orderStatusTYPE';
+import {cancelSubscribeOrder} from "/util/func/order/cancelOrder";
+import Spinner from "../../atoms/Spinner";
 
 
 export const SubscribeItems = ({ itemList }) => {
+  const [isLoading, setIsLoading] = useState( {} );
   // console.log(itemList);
+
+  const onCancelSubscribeOrder = useCallback(async (item) => {
+    if(!confirm("결제 전 주문을 결제취소처리 하시겠습니까?")) return;
+    
+    
+    const merchantUid = item.subscribeOrderDto.merchantUid;
+    const orderId = item.subscribeOrderDto.orderId;
+    try {
+      setIsLoading(prevState=> ({
+        ...prevState,
+        cancelOrder: true
+      }));
+      const res = await cancelSubscribeOrder(orderId);
+      console.log(res);
+      if ( res.isDone ) {
+        alert(`결제취소처리가 완료되었습니다. \n주문번호: ${merchantUid}`);
+        window.location.reload();
+      } else{
+        alert(`결제취소처리에 실패하였습니다.`);
+      }
+      
+    } catch (err) {
+        alert(`결제취소 처리 중 오류가 발생하였습니다.\n 지속적으로 에러가 발생할 경우, 관리자에게 문의바랍니다.`);
+        console.error(err)
+    } finally {
+      setIsLoading(prevState=> ({
+        ...prevState,
+        cancelOrder: false
+      }))
+    }
+  },[itemList]);
   
   return (
     <>
@@ -53,13 +86,17 @@ export const SubscribeItems = ({ itemList }) => {
                 </div>
 
                 <div className={s.right_box}>
-                  <Link
-                    href={`/mypage/orderHistory/subscribe/${item.subscribeOrderDto.orderId}`}
-                    passHref
-                  >
-                    <a className={s.btn}>주문상세 </a>
-                  </Link>
-                  {/*<a href={`/mypage/subscribe/2924`} className={s.btn2}>구독관리</a>*/}
+                  {item.subscribeOrderDto.orderStatus === orderStatus.BEFORE_PAYMENT
+                    ? <button className={`${s.btn} ${s.cancelOrder}`} onClick={onCancelSubscribeOrder.bind(null, item)}>
+                      {isLoading.cancelOrder ? <Spinner style={{color:"#fff"}}/> : "결제 취소"}
+                      </button>
+                    :  <Link
+                        href={`/mypage/orderHistory/subscribe/${item.subscribeOrderDto.orderId}`}
+                        passHref
+                      >
+                        <a className={s.btn}>주문상세 </a>
+                      </Link>
+                  }
                   <a
                     href={`/mypage/subscribe/${item.subscribeOrderDto.subscribeId}`}
                     className={s.btn2}
