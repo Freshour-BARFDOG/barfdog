c283e650ca473db812fb312b22c4bbb830ada45c
sbkim <victory289@naver.com>
2023-03-06 13:05:48 +0900

    #3 - 최소결제금액 로직 추가

--- src/components/order/calcOrdersheetPrices.js
+++ src/components/order/calcOrdersheetPrices.js
@@ -1,35 +1,39 @@
+import {IAMPORT_MIN_PAYMENT_PRICE} from "/store/TYPE/order/priceType";
+
 export const calcOrdersheetPrices = (form, orderType='general') => {
   
-  let discountCoupon = 0;
-  let discountReward = 0;
-  let discountGrade = 0;
+  const reward = form.selfInfo?.reward; // 유저 적립금 총액
+  let discountReward = Number(form.discountReward); // 적립금 할인
+  let discountCoupon = 0; // 쿠폰 할인
+  let discountGrade = Number(form.selfInfo?.discountGrade); // 등급 할인 ( ONLY 정기결제: 0 ~ 7%)
+  let deliveryPrice = Number(form.deliveryPrice); // 배송비
+  const orderPrice = Number( form.orderPrice ); // 상품금액
+  const minPaymentPrice = IAMPORT_MIN_PAYMENT_PRICE; // 최소 결제 금액
+  let overDiscountCoupon = 0; // 쿠폰의 초과할인금
+  
+  // console.log(discountReward, discountGrade, orderPrice);
+  if(isNaN(discountReward) || isNaN(discountGrade) || isNaN(orderPrice)) return null;
+  
   if(orderType === 'general'){
     discountCoupon = form.orderItemDtoList?.length ? form.orderItemDtoList.map( item => item.discountAmount ).reduce( (acc, cur) => acc + cur ) : 0;
-    discountReward = Number(form.discountReward);
-    // discountGrade = Number(form.selfInfo?.discountGrade); // 일반결제는 등급할인 없음
+    discountGrade = 0; // 일반결제는 등급할인 없음
   } else if ( orderType === 'subscribe') {
     discountCoupon = Number(form.discountCoupon);
-    discountReward = Number(form.discountReward);
-    discountGrade = Number(form.selfInfo?.discountGrade);
+    deliveryPrice = 0; // 정기결제는 항상 무료배송
   }
   
-  
-  const calc = Number(form.orderPrice) - discountCoupon - discountReward - discountGrade + form.deliveryPrice;
-  const paymentPrice = calc ? calc : 0;
-
-  const reward = form.selfInfo?.reward;
-  let availableMaxReward;
-  const minimumPaymentPrice = 100; // 아임포트 최소 결제금액 100원.
-  if(paymentPrice <= minimumPaymentPrice){
-    availableMaxReward = 0;
-  } else if (reward >= paymentPrice - minimumPaymentPrice)  {
-    availableMaxReward = paymentPrice - minimumPaymentPrice;
-  } else {
-    availableMaxReward = reward;
-  }
   
   const discountTotal = discountCoupon + discountReward + discountGrade;
-  // console.log('쿠폰할인 ',discountCoupon,'적립금할인',discountReward, '등급할인',discountGrade,  '할인총합', discountTotal, '결제금액: ', paymentPrice)
+  const calcedPaymentPrice = orderPrice + deliveryPrice - discountTotal ;
+  if ( calcedPaymentPrice < minPaymentPrice ) {
+    overDiscountCoupon = minPaymentPrice - calcedPaymentPrice;
+  }
+  
+  
+  
+  const paymentPrice = calcedPaymentPrice > minPaymentPrice ? calcedPaymentPrice : minPaymentPrice; // 23.02. 고객사 요청: 초과할인이 발생하더라도 결제 가능함.
+  const availableMaxDiscount = calcAvailableMaxReward(calcedPaymentPrice, minPaymentPrice, reward);
+  // console.log("---------------",'\n적립금할인: ',discountReward, '\n쿠폰할인: ',discountCoupon, '\n등급할인: ',discountGrade, '\n할인총합:', discountTotal, '\n배송비:',deliveryPrice, "\nORIGIN-결제금액: ",orderPrice, "\nCALCED-주문금액: ",calcedPaymentPrice,  '\nFINAL-결제금액: ', paymentPrice, "\n쿠폰 초과할인금액:",overDiscountCoupon,);
   
   return {
     discountReward,
@@ -37,7 +41,24 @@ export const calcOrdersheetPrices = (form, orderType='general') => {
     discountGrade,
     discountTotal,
     paymentPrice,
-    availableMaxReward
+    availableMaxDiscount: availableMaxDiscount,
+    overDiscountCoupon
   }
   
 }
+
+
+const calcAvailableMaxReward = (calcedPaymentPrice, minPaymentPrice, reward) => {
+  let result;
+  
+  if(calcedPaymentPrice <= minPaymentPrice){
+    result = calcedPaymentPrice - minPaymentPrice; // 변경: 적립금 input을 초기화시키기 위해, 음수까지 표현
+  } else if (reward >= calcedPaymentPrice - minPaymentPrice)  {
+    result = calcedPaymentPrice - minPaymentPrice;
+  } else {
+    result = reward;
+  }
+  
+  return result;
+  
+};
--- util/func/calculateSalePrice.js
+++ util/func/calculateSalePrice.js
@@ -1,6 +1,7 @@
 import transformClearLocalCurrency from './transformClearLocalCurrency';
 import transformLocalCurrency from './transformLocalCurrency';
 import {discountUnitType} from "/store/TYPE/discountUnitType";
+import {IAMPORT_MIN_PAYMENT_PRICE} from "../../store/TYPE/order/priceType";
 
 
 export default function calculateSalePrice (originPrice, discountType, discountDegree) {
@@ -28,16 +29,16 @@ export default function calculateSalePrice (originPrice, discountType, discountD
 
 
 export const calculateAndConvertToMinimumSalePrice = ({originPrice, discount = {coupon: 0, grade: 0, reward: 0, deliveryPrice:0}}) => {
-  const minimumPaymentPrice = 100; // 아임포트 최소 결제 금액
+  const minPaymentPrice = IAMPORT_MIN_PAYMENT_PRICE; // 아임포트 최소 결제 금액
   const coupon = discount.coupon || 0;
   const grade = discount.grade || 0;
   const reward = discount.reward || 0;
   const deliveryPrice = discount.deliveryPrice || 0;
   const calc = originPrice - coupon - grade - reward + deliveryPrice; // 배송비는 더한다.
-  const unappliedDiscountAmount = calc < minimumPaymentPrice ? minimumPaymentPrice - calc : 0; // 최소결제금액보다 할인금액이 클 경우, 할인 미적용 금액
+  const overDiscount = calc < minPaymentPrice ? minPaymentPrice - calc : 0; // 최소결제금액보다 할인금액이 클 경우, 할인 미적용 금액
   // console.log(coupon, grade, reward, unappliedDiscountAmount);
   return {
-    salePrice: calc >= minimumPaymentPrice ? calc : minimumPaymentPrice,
-    unappliedDiscountAmount: unappliedDiscountAmount
+    salePrice: calc >= minPaymentPrice ? calc : minPaymentPrice,
+    overDiscount: overDiscount
   };
 };
