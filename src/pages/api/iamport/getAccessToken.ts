092631c68f9fbbe5217b9087ace0253f5ddd6259
sbkim <victory289@naver.com>
2023-06-23 18:44:42 +0900

    #26 - 네이버 페이 - 결제내역 사전정보 등록 & 검증기능 추가 (iamport API)
    - I'mport API: https://api.iamport.kr/#/
    - Implementation Notes
     (아임포트 javascript사용)인증방식의 결제를 진행할 때 결제금액 위변조시 결제진행자체를 block하기 위해 결제예정금액을 사전등록하는 기능입니다.
     이 API를 통해 사전등록된 가맹점 주문번호(merchant_uid)에 대해, IMP.request_pay()에 전달된 merchant_uid가 일치하는 주문의 결제금액이 다른 경우 PG사 결제창 호출이 중단됩니다.

---
+++ src/pages/api/iamport/getAccessToken.ts
@@ -0,0 +1,27 @@
+import {axiosOfIamport} from "@src/pages/api/iamport/axiosOfIamport";
+import {AxiosResponse} from "axios";
+
+export interface IamportTokenResponseInterface {
+  access_token: string;
+  expired_at: number;
+}
+
+export default async function POST(req:Request, res:Response) {
+
+  const tokenRes:AxiosResponse = await axiosOfIamport.post('/users/getToken', {
+    imp_key: `${process.env.NEXT_PUBLIC_IAMPORT_REST_API_KEY}`,
+    imp_secret: `${process.env.NEXT_PUBLIC_IAMPORT_REST_API_SECRET}`
+  });
+  const {access_token, expired_at} = tokenRes.data.response;
+  const body: IamportTokenResponseInterface = {
+    access_token, expired_at
+  }
+
+  if (!access_token) {
+    res.status(500).json({
+      message: "Failed to get Iamport access token."
+    });
+  }
+
+  return res.status(200).json(body);
+}
