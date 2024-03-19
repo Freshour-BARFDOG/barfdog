498da0ac8d0dfb4f62f10eac8e3ce635e20a5f46
saywhat <love7620640@naver.com>
2024-03-19 11:52:21 +0900

    Feat: Meta Pixel 추가
    Resolves: #TSK-57

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -66,6 +66,26 @@ export default function CustomDocument() {
           `,
           }}
         />
+        {/* <!-- Meta Pixel Code -->  */}
+        <script
+          data-script-title={'META PIXEL'}
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1294667177830818');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        {/* <!-- End Meta Pixel Code --> */}
+
         {/* <!-- Google Tag Manager --> */}
         <script
           data-script-title={'GOOGLE-TAG-MANAGER'}
@@ -86,18 +106,26 @@ export default function CustomDocument() {
         <MetaTags />
       </Head>
       <body>
+        <noscript>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PG8PX9K9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
+          {/* <!-- Meta Pixel Code (noscript) -->  */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1294667177830818&ev=PageView&noscript=1"
+          />
+          {/* <!-- End Meta Pixel Code (noscript) --> */}
+        </noscript>
         <Main />
       </body>
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-PG8PX9K9"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
-      {/* <!-- End Google Tag Manager (noscript) --> */}
       <NextScript />
     </Html>
   );
