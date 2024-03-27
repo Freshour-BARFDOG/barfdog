8a822be33c33baa0a2c6b684c94c6303f672e18d
saywhat <love7620640@naver.com>
2024-03-27 17:00:24 +0900

    Test: 생년월일 자동기입 확인

--- src/components/user_signup/SignInput_birthday.jsx
+++ src/components/user_signup/SignInput_birthday.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
-import s from "/src/pages/account/signup/signup.module.scss";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { subYears, endOfYear, getYear, getMonth, format } from "date-fns";
-import range from "lodash/range";
-import ko from "date-fns/locale/ko";
-import { FaRegCalendarAlt } from "react-icons/fa";
-import { FcNext } from "react-icons/fc";
-import { FcPrevious } from "react-icons/fc";
+import React, { useEffect, useState, useRef } from 'react';
+import s from '/src/pages/account/signup/signup.module.scss';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { subYears, endOfYear, getYear, getMonth, format } from 'date-fns';
+import range from 'lodash/range';
+import ko from 'date-fns/locale/ko';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { FcNext } from 'react-icons/fc';
+import { FcPrevious } from 'react-icons/fc';
 const SignInput_birthday = ({
   type,
   required,
@@ -24,42 +24,40 @@ const SignInput_birthday = ({
   icon,
   ...props
 }) => {
-  const initialValue = formValue || "";
-  const [selectedDate, setSelectedDate] = useState(null);
+  const initialValue = formValue || '';
+  const [selectedDate, setSelectedDate] = useState(initialValue);
 
   const handleRawInput = (e) => {
-    const rawInput = e.target.value 
+    const rawInput = e.target.value;
     // 입력값이 'yyyyMMdd' 형식과 일치하는지 확인
     const dateIsValid = rawInput?.match(/^(\d{4}).?(\d{2}).?(\d{2})$/);
     if (dateIsValid) {
       const formattedDate = `${dateIsValid[1]}-${dateIsValid[2]}-${dateIsValid[3]}`;
       handleDateChange(new Date(formattedDate));
-    } 
-
-  }
+    }
+  };
 
   const handleDateChange = (date) => {
-
-    let formattedDate = ""
+    let formattedDate = '';
 
     // date 값이 유효하면, 날짜 포맷 변경
-    if (date && !isNaN(date)) { 
-        formattedDate = format(date, "yyyy-MM-dd");
-        setSelectedDate(date);
-        setFormValues((prevState) => {
-          return {
-            ...prevState,
-            [id]: formattedDate,
-          };
-        });
-      }
-  
+    if (date && !isNaN(date)) {
+      formattedDate = format(date, 'yyyy-MM-dd');
+      setSelectedDate(date);
+      setFormValues((prevState) => {
+        return {
+          ...prevState,
+          [id]: formattedDate,
+        };
+      });
+    }
+
     if (date === null) {
       setSelectedDate(null);
       setFormValues((prevState) => {
         return {
           ...prevState,
-          [id]: "",
+          [id]: '',
         };
       });
       return;
@@ -76,7 +74,7 @@ const SignInput_birthday = ({
   };
 
   const addedClassNameList = () => {
-    let classNameList = "";
+    let classNameList = '';
     if (!addedClassName?.length) return;
 
     addedClassName?.forEach((list) => {
@@ -92,18 +90,18 @@ const SignInput_birthday = ({
   const [startDate, setStartDate] = useState(new Date());
   const years = range(1900, getYear(new Date()) + 1, 1).reverse();
   const months = [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월",
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
   ];
 
   const [isYearDropdownOpen, setIsYearDropdownOpen] = useState(false);
@@ -121,116 +119,117 @@ const SignInput_birthday = ({
 
   const selectedDayClassName = (date) =>
     selectedDate && date.getTime() === selectedDate.getTime()
-      ? (`${s.day}`)
+      ? `${s.day}`
       : null;
 
-
   const handleInputFocus = (event) => {
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
+    const isMobileDevice =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent,
+      );
 
     if (isMobileDevice) {
       event.target.blur(); // 모바일 장치에서만 포커스 해제
     }
   };
 
-
   return (
     <>
-      <div className={s["join__wrap"]}>
-        <div className={s["input-title-wrap"]}>
+      <div className={s['join__wrap']}>
+        <div className={s['input-title-wrap']}>
           <label htmlFor={id}>
-            <span className={`${s["inp-title"]} ${required && s["required"]}`} id={`${id}Title`}>
+            <span
+              className={`${s['inp-title']} ${required && s['required']}`}
+              id={`${id}Title`}
+            >
               {title}
             </span>
           </label>
         </div>
-        <div className={`${s["input-cont-wrap"]} ${addedClassNameList()}`}>
+        <div className={`${s['input-cont-wrap']} ${addedClassNameList()}`}>
           <div className={s['input-wrap']}>
-          <div className={s.date_wrap}>
-            <div className={`${s.svg_wrap} img-wrap`}>
-              <FaRegCalendarAlt className={s["icon"]} size="100%" />
-            </div>    
+            <div className={s.date_wrap}>
+              <div className={`${s.svg_wrap} img-wrap`}>
+                <FaRegCalendarAlt className={s['icon']} size="100%" />
+              </div>
 
-            <DatePicker
-              placeholderText="연도. 월. 일."
-              className={s["birthday"]}
-              id={id}
-              selected={selectedDate}
-              onChange={handleDateChange}
-              onChangeRaw={handleRawInput}
-              dateFormat={"yyyy. MM. dd."}
-              maxDate={maxDate}
-              required={required}
-              locale={ko}
-              disabledKeyboardNavigation
-              isClearable
-              // onFoucus 직접입력 막기-> MacOS Safari 정지 이슈발생
-              // PC환경 직접입력 허용(ex 2023. 05. 12. 폼으로 정확해야만 직접 입력됨, 달력에서 고를 수 없는 값은 입력불가, console 입력 확인완료)
-              // onFocus={e => e.target.blur()}
-              onFocus={handleInputFocus}
-              dayClassName={selectedDayClassName}
-              renderCustomHeader={({
-                date,
-                changeYear,
-                changeMonth,
-                decreaseMonth,
-                increaseMonth,
-                prevMonthButtonDisabled,
-                nextMonthButtonDisabled,
-              }) => (
-                <div
-                  style={{
-                    margin: 10,
-                    display: "flex",
-                    justifyContent: "center",
-                    columnGap: 10,
-                  }}
-                >
-                   <button 
-                    className={s.date_prev}
-                    onClick={decreaseMonth} 
-                    disabled={prevMonthButtonDisabled}
-                   >
-                    <FcPrevious />
-                  </button>
-
-                  <div 
+              <DatePicker
+                placeholderText="연도. 월. 일."
+                className={s['birthday']}
+                id={id}
+                selected={selectedDate}
+                onChange={handleDateChange}
+                onChangeRaw={handleRawInput}
+                dateFormat={'yyyy. MM. dd.'}
+                maxDate={maxDate}
+                required={required}
+                locale={ko}
+                disabledKeyboardNavigation
+                isClearable
+                // onFoucus 직접입력 막기-> MacOS Safari 정지 이슈발생
+                // PC환경 직접입력 허용(ex 2023. 05. 12. 폼으로 정확해야만 직접 입력됨, 달력에서 고를 수 없는 값은 입력불가, console 입력 확인완료)
+                // onFocus={e => e.target.blur()}
+                onFocus={handleInputFocus}
+                dayClassName={selectedDayClassName}
+                renderCustomHeader={({
+                  date,
+                  changeYear,
+                  changeMonth,
+                  decreaseMonth,
+                  increaseMonth,
+                  prevMonthButtonDisabled,
+                  nextMonthButtonDisabled,
+                }) => (
+                  <div
                     style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      rowGap: 10,
+                      margin: 10,
+                      display: 'flex',
+                      justifyContent: 'center',
+                      columnGap: 10,
                     }}
                   >
-                    <YearDropdown
-                      years={years}
-                      selectedYear={getYear(date)}
-                      onChange={(year) => changeYear(year.toString())}
-                      isOpen={isYearDropdownOpen}
-                      toggleDropdown={toggleYearDropdown}
-                    />
+                    <button
+                      className={s.date_prev}
+                      onClick={decreaseMonth}
+                      disabled={prevMonthButtonDisabled}
+                    >
+                      <FcPrevious />
+                    </button>
 
-                    <MonthDropdown
-                      months={months}
-                      selectedMonth={months[getMonth(date)]}
-                      onChange={(month) => changeMonth(months.indexOf(month))}
-                      isOpen={isMonthDropdownOpen}
-                      toggleDropdown={toggleMonthDropdown}
-                    />
-                  
-                  </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        rowGap: 10,
+                      }}
+                    >
+                      <YearDropdown
+                        years={years}
+                        selectedYear={getYear(date)}
+                        onChange={(year) => changeYear(year.toString())}
+                        isOpen={isYearDropdownOpen}
+                        toggleDropdown={toggleYearDropdown}
+                      />
 
-                  <button 
-                    className={s.date_next}
-                    onClick={increaseMonth} 
-                    disabled={nextMonthButtonDisabled}
-                  >
+                      <MonthDropdown
+                        months={months}
+                        selectedMonth={months[getMonth(date)]}
+                        onChange={(month) => changeMonth(months.indexOf(month))}
+                        isOpen={isMonthDropdownOpen}
+                        toggleDropdown={toggleMonthDropdown}
+                      />
+                    </div>
+
+                    <button
+                      className={s.date_next}
+                      onClick={increaseMonth}
+                      disabled={nextMonthButtonDisabled}
+                    >
                       <FcNext />
-                  </button>
-                </div>
-              )}
-            />
+                    </button>
+                  </div>
+                )}
+              />
             </div>
             {icon && icon}
             {errorMessage}
@@ -244,11 +243,13 @@ const SignInput_birthday = ({
 
 export default SignInput_birthday;
 
-
-
-
-
-function YearDropdown({ years, selectedYear, onChange, isOpen, toggleDropdown }) {
+function YearDropdown({
+  years,
+  selectedYear,
+  onChange,
+  isOpen,
+  toggleDropdown,
+}) {
   // const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
@@ -256,7 +257,7 @@ function YearDropdown({ years, selectedYear, onChange, isOpen, toggleDropdown })
     if (isOpen && dropdownRef.current) {
       // Scroll to the selected year's position when the dropdown is opened
       const selectedYearButton = dropdownRef.current.querySelector(
-        `button[value="${selectedYear}"]`
+        `button[value="${selectedYear}"]`,
       );
       if (selectedYearButton) {
         const selectedYearRect = selectedYearButton.getBoundingClientRect();
@@ -288,10 +289,10 @@ function YearDropdown({ years, selectedYear, onChange, isOpen, toggleDropdown })
                 // setIsOpen(false);
                 toggleDropdown();
               }}
-              style={{ 
-                fontWeight: year === selectedYear ? "600" : "normal", 
-                backgroundColor: year === selectedYear ? "#efefef" : "#fff", 
-                color: year === selectedYear ? "#ca1011" : "#000", 
+              style={{
+                fontWeight: year === selectedYear ? '600' : 'normal',
+                backgroundColor: year === selectedYear ? '#efefef' : '#fff',
+                color: year === selectedYear ? '#ca1011' : '#000',
               }}
             >
               {year}
@@ -302,7 +303,13 @@ function YearDropdown({ years, selectedYear, onChange, isOpen, toggleDropdown })
     </div>
   );
 }
-function MonthDropdown({ months, selectedMonth, onChange, isOpen, toggleDropdown }) {
+function MonthDropdown({
+  months,
+  selectedMonth,
+  onChange,
+  isOpen,
+  toggleDropdown,
+}) {
   // const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
@@ -310,7 +317,7 @@ function MonthDropdown({ months, selectedMonth, onChange, isOpen, toggleDropdown
     if (isOpen && dropdownRef.current) {
       // Scroll to the selected month's position when the dropdown is opened
       const selectedMonthButton = dropdownRef.current.querySelector(
-        `button[value="${selectedMonth}"]`
+        `button[value="${selectedMonth}"]`,
       );
       if (selectedMonthButton) {
         const selectedMonthRect = selectedMonthButton.getBoundingClientRect();
@@ -331,7 +338,7 @@ function MonthDropdown({ months, selectedMonth, onChange, isOpen, toggleDropdown
         {selectedMonth}
       </button>
       {isOpen && (
-         <div className={s.pic_flex} ref={dropdownRef}>
+        <div className={s.pic_flex} ref={dropdownRef}>
           {months.map((month) => (
             <button
               className={s.pic_list}
@@ -342,10 +349,10 @@ function MonthDropdown({ months, selectedMonth, onChange, isOpen, toggleDropdown
                 // setIsOpen(false);
                 toggleDropdown();
               }}
-              style={{ 
-                fontWeight: month === selectedMonth ? "600" : "normal",
-                backgroundColor: month === selectedMonth ? "#efefef" : "#fff", 
-                color: month === selectedMonth ? "#ca1011" : "#000", 
+              style={{
+                fontWeight: month === selectedMonth ? '600' : 'normal',
+                backgroundColor: month === selectedMonth ? '#efefef' : '#fff',
+                color: month === selectedMonth ? '#ca1011' : '#000',
               }}
             >
               {month}
