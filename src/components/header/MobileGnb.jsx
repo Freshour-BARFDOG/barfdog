d74e3860aea18ca86bd3ca079c4ecaeb55eca213
saywhat <love7620640@naver.com>
2024-04-01 15:53:20 +0900

    Feat: 메인페이지 리뉴얼 배너 크기 수정
    Ref: #TSK-67

--- src/components/header/MobileGnb.jsx
+++ src/components/header/MobileGnb.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import s from './mobileGnb.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useModalContext } from '/store/modal-context';
 import useDeviceState from '/util/hook/useDeviceState';
 import { IoIosArrowDown } from 'react-icons/io';
@@ -16,7 +17,7 @@ export default function MobileGnb() {
   const mcx = useModalContext();
 
   const deviceState = useDeviceState();
-  const [activeMenuId, setActiveMenuId] = useState('');
+  const [activeMenuId, setActiveMenuId] = useState('홈');
   const [isWideMode, setIsWideMode] = useState(false);
   const deviceWidth = deviceState.deviceWidth;
 
@@ -44,6 +45,10 @@ export default function MobileGnb() {
     setIsWideMode(!isWideMode);
   };
 
+  useEffect(() => {
+    console.log(setActiveMenuId);
+  }, []);
+
   return (
     <>
       <nav className={`${s.mobileNav} mobile`} ref={curMenuRef}>
@@ -53,37 +58,119 @@ export default function MobileGnb() {
           }`}
         >
           <ul className={`${s['mobile-menu']} `}>
-            <MobileMenu title={'정기구독'} fakeLink="/survey" onClick={onShowModal} />
-            <MobileMenu
-              title={'샵'}
-              fakeLink="/shop"
-              id={menuNameObj.shop}
-              onClick={onActiveSubmenuHandler}
-            />
-            <MobileMenu title={'레시피'} link={'/recipes'} />
-            <MobileMenu
+            <li>
+              <Link href="/" passHref>
+                <a onClick={() => setActiveMenuId('홈')}>
+                  <Image
+                    src={
+                      activeMenuId === '홈'
+                        ? '/img/icon/mobile-home-active.svg'
+                        : '/img/icon/mobile-home.svg'
+                    }
+                    alt="home"
+                    width={30}
+                    height={30}
+                  />
+                  <MobileMenu
+                    title={'홈'}
+                    link="/"
+                    activeMenuId={activeMenuId}
+                  />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/survey" passHref>
+                <a onClick={() => setActiveMenuId('AI 추천식단')}>
+                  <Image
+                    src={
+                      activeMenuId === 'AI 추천식단'
+                        ? '/img/icon/mobile-subscribe-active.svg'
+                        : '/img/icon/mobile-subscribe.svg'
+                    }
+                    alt="subscribe"
+                    width={40}
+                    height={40}
+                  />
+                  <MobileMenu
+                    title={'AI 추천식단'}
+                    link="/survey"
+                    activeMenuId={activeMenuId}
+                  />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/shop?itemType=${general_itemType.ALL}`} passHref>
+                <a onClick={() => setActiveMenuId('스토어')}>
+                  <Image
+                    src={
+                      activeMenuId === '스토어'
+                        ? '/img/icon/mobile-store-active.svg'
+                        : '/img/icon/mobile-store.svg'
+                    }
+                    alt="store"
+                    width={30}
+                    height={30}
+                  />
+                  <MobileMenu
+                    title={'스토어'}
+                    link={`/shop?itemType=${general_itemType.ALL}`}
+                    // fakeLink="/shop"
+                    // id={menuNameObj.shop}
+                    onClick={onActiveSubmenuHandler}
+                    activeMenuId={activeMenuId}
+                  />
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Image
+                src="/img/icon/mobile-mypage.svg"
+                alt="shop"
+                width={30}
+                height={30}
+              />
+              <MobileMenu title={'MY'} link="/mypage" />
+            </li>
+            {/* <MobileMenu title={'레시피'} link={'/recipes'} /> */}
+            {/* <MobileMenu
               title={'커뮤니티'}
               fakeLink="/community"
               id={menuNameObj.community}
               onClick={onActiveSubmenuHandler}
-            />
-            <MobileMenu title={'리뷰'} link="/review" />
+            /> */}
+            {/* <MobileMenu title={'리뷰'} link="/review" /> */}
           </ul>
         </section>
         <section
           className={`${s['mobile-submenu-wrap']} mobile-submenu-wrap ${
             activeMenuId ? s.active : ''
-          } ${isWideMode ? s.widemode : ''} ${deviceWidth < 300 ? s['scroll-container'] : ''}`}
+          } ${isWideMode ? s.widemode : ''} ${
+            deviceWidth < 300 ? s['scroll-container'] : ''
+          }`}
         >
           <ul
             className={`${s['mobile-submenu']}  ${
               activeMenuId === menuNameObj.shop ? s.active : ''
             }`}
           >
-            <MobileMenu title="ALL" link={`/shop?itemType=${general_itemType.ALL}`} />
-            <MobileMenu title="생식" link={`/shop?itemType=${general_itemType.RAW}`} />
-            <MobileMenu title="토핑" link={`/shop?itemType=${general_itemType.TOPPING}`} />
-            <MobileMenu title="굿즈" link={`/shop?itemType=${general_itemType.GOODS}`} />
+            <MobileMenu
+              title="ALL"
+              link={`/shop?itemType=${general_itemType.ALL}`}
+            />
+            <MobileMenu
+              title="생식"
+              link={`/shop?itemType=${general_itemType.RAW}`}
+            />
+            <MobileMenu
+              title="토핑"
+              link={`/shop?itemType=${general_itemType.TOPPING}`}
+            />
+            <MobileMenu
+              title="굿즈"
+              link={`/shop?itemType=${general_itemType.GOODS}`}
+            />
           </ul>
           <ul
             className={`${s['mobile-submenu']} ${
@@ -108,13 +195,14 @@ const currentPageIndicator = (ref, curPath, setActivemenu) => {
   if (!ref) return;
   const nav = ref;
   const curPathArray = curPath.split('/');
-  const curPageDepth1 = curPath.indexOf('?') >= 0 ? curPath?.split('?')[0] : curPathArray[1];
+  const curPageDepth1 =
+    curPath.indexOf('?') >= 0 ? curPath?.split('?')[0] : curPathArray[1];
   const curPageDepth2 = curPathArray[2];
 
   const mainMenus = Array.from(
     nav.querySelectorAll(`.mobile-menu-wrap a, .mobile-menu-wrap button`),
   );
-  
+
   mainMenus.forEach((mainmenu) => {
     const thisMenuId = mainmenu.id;
     const menuPath = mainmenu.pathname || mainmenu.dataset.link;
@@ -134,8 +222,9 @@ const currentPageIndicator = (ref, curPath, setActivemenu) => {
     if (!menuPath && curPathArray) return;
     // // console.log(menuPath)
     // // console.log(curPageDepth2)
-    submenu.dataset.currentPage = menuPath.indexOf(curPageDepth2) >= 0 && 'depth2'
-    
+    submenu.dataset.currentPage =
+      menuPath.indexOf(curPageDepth2) >= 0 && 'depth2';
+
     if (curPath.indexOf('/shop') >= 0) {
       const keyword = 'itemType=';
       let submenuSearch = submenu.search.split(keyword)[1];
@@ -144,23 +233,42 @@ const currentPageIndicator = (ref, curPath, setActivemenu) => {
         submenuSearch = curUrlSearch?.split('&')[0];
         curUrlSearch = curUrlSearch?.split('&')[0];
       }
-      submenu.dataset.currentPage = curUrlSearch?.indexOf(submenuSearch) >= 0 && 'depth2';
+      submenu.dataset.currentPage =
+        curUrlSearch?.indexOf(submenuSearch) >= 0 && 'depth2';
     }
   });
-
 };
 
-const MobileMenu = ({ onClick, link, fakeLink, id, title }) => {
+const MobileMenu = ({
+  onClick,
+  link,
+  fakeLink,
+  id,
+  title,
+  color,
+  activeMenuId,
+}) => {
   const onClickHandler = () => {
     if (onClick && typeof onClick === 'function') {
       onClick(id);
     }
   };
+
+  useEffect(() => {
+    console.log('activeMenuId', activeMenuId);
+    console.log('title', title);
+  }, []);
+
   return (
     <li onClick={onClick && onClickHandler}>
       {link ? (
         <Link href={link} passHref>
-          <a id={id}>{title}</a>
+          <a
+            id={id}
+            style={{ color: activeMenuId === title ? '#cb1010' : '#535353' }}
+          >
+            {title}
+          </a>
         </Link>
       ) : (
         <button id={id} data-link={fakeLink} type={'button'}>
