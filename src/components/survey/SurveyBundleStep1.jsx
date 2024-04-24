2b2fd14d6c8b5da9c03ce386f36a8e1a5d118696
saywhat <love7620640@naver.com>
2024-04-24 15:44:26 +0900

    Feat: 관리자 반려견관리 상세조회 팝업창에서 유저 데이터 수정
    [BEFORE]
    
    /api/dogs/{id} 에서
    유저정보
    String memberName;
    String email;
    Address address;
    String phoneNumber;
    String memberBirthday;
    데이터 조회
    
    [AFTER]
    /api/dogs/{id} 에서
    유저정보 Long memberId; 만 데이터 가져오고,
    나머지 유저정보는 /api/admin/members/{memberId} 로 데이터 조회하게 수정함

--- src/components/survey/SurveyBundleStep1.jsx
+++ src/components/survey/SurveyBundleStep1.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import s from '/src/pages/survey/survey.module.scss';
-import SurveyInputRadio from '/src/components/survey/SurveyInputRadio';
+import SurveyInputRadio from '/src/components/survey/AdminSurveyInputRadio';
 import { CustomSelectWithCustomOptions } from './CustomSelectWithCustomOptions';
 import { DogTypeCustomSelectWithCustomOptions } from './DogTypeCustomSelectWithCustomOptions';
 import PureCheckbox from '/src/components/atoms/PureCheckbox';
