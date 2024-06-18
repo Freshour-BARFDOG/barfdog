f3f8ddbceca70ec46d956ae76fd931585001daa9
saywhat <love7620640@naver.com>
2024-06-18 12:55:25 +0900

    Feat: [update] 메인페이지 인트로 및 푸터 추가

---
+++ src/components/home/Intro_top.jsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import React from 'react';
+import s from 'src/pages/mainPage.module.scss';
+import LogoMain from '/public/img/logo_main.png';
+import Image from 'next/image';
+
+export function Intro_top() {
+  // 1. 영상 소개
+  return (
+    <section className={s.intro_top_wrapper}>
+      <div className={s.inner}>
+        <div className={s.text_box}>
+          <h1>
+            우리는 사료가 아닌
+            <br /> 음식을 만듭니다
+          </h1>
+          <h2>보다 나은 견생을 위한 선택</h2>
+          <div className={s.image_wrap}>
+            <Image
+              src={require('/public/img/logo_main.png')}
+              alt="카드 이미지"
+            />
+          </div>
+        </div>
+        <div className={s.btn_box}>
+          <Link href={'/surveyGuide'} passHref>
+            <a type="button" className={s.btn_survey}>
+              설문하고
+              <br /> 추천받기
+            </a>
+          </Link>
+          <Link href={'/surveyGuide'} passHref>
+            <a type="button" className={s.btn_score}>
+              건강점수 <br />
+              비교하기
+            </a>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
