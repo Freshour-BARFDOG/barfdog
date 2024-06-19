bee0e0b0e894385f27826eedfd9e4255f2ce93e3
saywhat <love7620640@naver.com>
2024-06-19 12:55:58 +0900

    Feat: [관리자] 적립금 조회 페이지 변경요청
    관리자 > 적립금관리> 적립금조회 > 적립금목록 표 수정
    
    - 컬럼명 변경
        - 회원이름 → 적립금 수령자
        - 아이디 → 수령자 아이디적리
    
    - 컬럼 추가
        - 적립금 타입
        - 적립금 발급자
        - 발급자 아이디
    
    - 검색 조건 추가
        - 적립금 타입 체크박스 생성
        - RECOMMEND(친구추천), FRIEND_PAY(친구첫구매), REVIEW(리뷰), EVENT(이벤트), ORDER(주문), SUBSCRIBE(구독), ADMIN(관리자), RECEIVE(수신동의)
        - 체크박스 한글 이름으로 표시, Default는 전체선택 안한상태, 다중선택 가능
    
    Resolves: #TSK-103

--- src/pages/bf-admin/reward/search/RewardList.jsx
+++ src/pages/bf-admin/reward/search/RewardList.jsx
@@ -2,6 +2,7 @@ import s from '../reward.module.scss';
 import transformLocalCurrency from '/util/func/transformLocalCurrency';
 import transformDate from '/util/func/transformDate';
 import { rewardStatusType } from '../../../../../store/TYPE/rewardStatusType';
+import { rewardType } from '../../../../../store/TYPE/rewardType';
 
 const ItemList = ({ item, number }) => {
   const DATA = {
@@ -9,9 +10,12 @@ const ItemList = ({ item, number }) => {
     number,
     createdDate: transformDate(item.createdDate), // 발행일자
     name: item.name, // 적립금 발행 시, 발행적립금 타이틀
+    rewardType: rewardType.KOR[item.rewardType], // 적립금 타입
     amount: transformLocalCurrency(item.amount) + '원', // 발행 금액
-    memberName: item.memberName, // 회원명
-    email: item.email,
+    memberName: item.memberName, // 적립금 발급자 (회원명)
+    email: item.email, // 발급자 아이디
+    issuerName: item.issuerName, // 적립금 수령자
+    issuerEmail: item.issuerEmail, // 수령자 아이디
     rewardStatus: item.rewardStatus,
   };
 
@@ -20,12 +24,15 @@ const ItemList = ({ item, number }) => {
       <span>{DATA.number}</span>
       <span>{DATA.createdDate}</span>
       <span>{DATA.name}</span>
+      <span>{DATA.rewardType}</span>
       <span>
         {item.rewardStatus === rewardStatusType.SAVED ? '+' : '-'}
         {DATA.amount}
       </span>
       <span>{DATA.memberName}</span>
       <span>{DATA.email}</span>
+      <span>{DATA.issuerName}</span>
+      <span>{DATA.issuerEmail}</span>
     </li>
   );
 };
--- src/pages/bf-admin/reward/search/index.jsx
+++ src/pages/bf-admin/reward/search/index.jsx
@@ -5,6 +5,7 @@ import AdminLayout from '/src/components/admin/AdminLayout';
 import { AdminContentWrapper } from '/src/components/admin/AdminWrapper';
 import SearchBar from '/src/components/admin/form/searchBar';
 import SearchTerm from '/src/components/admin/form/searchBar/SearchTerm';
+import SearchCheckbox from '/src/components/admin/form/searchBar/SearchCheckbox';
 import SearchTextWithCategory from '/src/components/admin/form/searchBar/SearchTextWithCategory';
 import AmdinErrorMessage from '/src/components/atoms/AmdinErrorMessage';
 import RewardList from './RewardList';
@@ -14,12 +15,14 @@ import Spinner from '/src/components/atoms/Spinner';
 import enterKey from '/util/func/enterKey';
 import { getDefaultPagenationInfo } from '/util/func/getDefaultPagenationInfo';
 import { global_searchDateType } from '/store/TYPE/searchDateType';
+import { rewardType } from '/store/TYPE/rewardType';
 
 const initialSearchValues = {
   email: '',
   name: '',
   from: global_searchDateType.oldestDate,
   to: transformToday(),
+  rewardTypeList: Object.keys(rewardType).filter((key) => key !== 'KOR'),
 };
 
 function RewardListPage() {
@@ -27,10 +30,16 @@ function RewardListPage() {
   const searchPageSize = 10;
   const [isLoading, setIsLoading] = useState({});
   const [itemList, setItemList] = useState([]);
-  const [searchValues, setSearchValues] = useState(initialSearchValues);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchQueryInitialize, setSearchQueryInitialize] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isNone, setIsNone] = useState(false);
+  const [searchValues, setSearchValues] = useState(initialSearchValues);
+
+  const initialValue =
+    searchValues.rewardType ||
+    Object.keys(rewardType).filter((key) => key != 'KOR');
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState(initialValue);
 
   const pageInterceptor = useCallback((res, option = { itemQuery: null }) => {
     // res = DUMMY__RESPONSE; // ! TEST
@@ -41,24 +50,36 @@ function RewardListPage() {
     });
   }, []);
 
+  // 초기화
   const onResetSearchValues = () => {
     setSearchValues(initialSearchValues);
+    setSelectedCheckboxes(
+      Object.keys(rewardType).filter((key) => key != 'KOR'),
+    );
   };
 
   const onSearchHandler = () => {
     const queryArr = [];
     for (const key in searchValues) {
       const val = searchValues[key];
+      // 선택 안함
+      if (key === 'rewardTypeList' && Array.isArray(val) && val.length === 0) {
+        return setIsNone(true);
+      }
       queryArr.push(`${key}=${val}`);
     }
 
     const query = `${queryArr.join('&')}`;
     setSearchQuery(query);
+    setIsNone(false);
   };
 
   const onSearchInputKeydown = (e) => {
     enterKey(e, onSearchHandler);
   };
+
+  // console.log('itemList', itemList);
+  // console.log('searchValues', searchValues);
 
   return (
     <>
@@ -87,6 +108,17 @@ function RewardListPage() {
                   { label: '이름', value: 'name' },
                 ]}
               />
+              <SearchCheckbox
+                searchValue={searchValues}
+                setSearchValue={setSearchValues}
+                title="적립금 타입"
+                name="rewardTypeList"
+                idList={Object.keys(rewardType).filter((key) => key != 'KOR')}
+                labelList={Object.values(rewardType.KOR)}
+                value={searchValues.rewardType}
+                selectedCheckboxes={selectedCheckboxes}
+                setSelectedCheckboxes={setSelectedCheckboxes}
+              />
             </SearchBar>
           </section>
           <section className="cont">
@@ -99,12 +131,15 @@ function RewardListPage() {
                 <ul className={s.table_header}>
                   <li className={s.table_th}>번호</li>
                   <li className={s.table_th}>적립일자</li>
-                  <li className={s.table_th}>적립급명칭</li>
+                  <li className={s.table_th}>적립급 명칭</li>
+                  <li className={s.table_th}>적립금 타입</li>
                   <li className={s.table_th}>금액</li>
-                  <li className={s.table_th}>회원이름</li>
-                  <li className={s.table_th}>아이디</li>
+                  <li className={s.table_th}>적립금 수령자</li>
+                  <li className={s.table_th}>수령자 아이디</li>
+                  <li className={s.table_th}>적립금 발급자</li>
+                  <li className={s.table_th}>발급자 아이디</li>
                 </ul>
-                {itemList.length ? (
+                {itemList.length && !isNone ? (
                   <RewardList items={itemList} currentPage={currentPage} />
                 ) : isLoading.fetching ? (
                   <AmdinErrorMessage loading={<Spinner />} />
@@ -113,18 +148,24 @@ function RewardListPage() {
                 )}
               </div>
             </div>
-            <div className={s['pagination-section']}>
-              <PaginationWithAPI
-                apiURL={getListApiUrl}
-                size={searchPageSize}
-                setItemList={setItemList}
-                urlQuery={searchQuery}
-                setIsLoading={setIsLoading}
-                pageInterceptor={pageInterceptor}
-                option={{ apiMethod: 'GET', initialize: searchQueryInitialize }}
-                setCurrentPage={setCurrentPage}
-              />
-            </div>
+
+            {!isNone && (
+              <div className={s['pagination-section']}>
+                <PaginationWithAPI
+                  apiURL={getListApiUrl}
+                  size={searchPageSize}
+                  setItemList={setItemList}
+                  urlQuery={searchQuery}
+                  setIsLoading={setIsLoading}
+                  pageInterceptor={pageInterceptor}
+                  option={{
+                    apiMethod: 'GET',
+                    initialize: searchQueryInitialize,
+                  }}
+                  setCurrentPage={setCurrentPage}
+                />
+              </div>
+            )}
           </section>
         </AdminContentWrapper>
       </AdminLayout>
