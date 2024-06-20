f3c0ee7eec0db0ba2e7f4904e90c0b29098a5a32
saywhat <love7620640@naver.com>
2024-06-20 17:26:03 +0900

    Feat: 메인>레이아웃 모바일로 통합 및 영상 추가

--- src/components/home/Intro_top.jsx
+++ src/components/home/Intro_top.jsx
@@ -9,32 +9,37 @@ export function Intro_top() {
   return (
     <section className={s.intro_top_wrapper}>
       <div className={s.inner}>
-        <div className={s.text_box}>
-          <h1>
-            우리는 사료가 아닌
-            <br /> 음식을 만듭니다
-          </h1>
-          <h2>보다 나은 견생을 위한 선택</h2>
-          <div className={s.image_wrap}>
-            <Image
-              src={require('/public/img/logo_main.png')}
-              alt="카드 이미지"
-            />
-          </div>
-        </div>
-        <div className={s.btn_box}>
-          <Link href={'/surveyGuide'} passHref>
-            <a type="button" className={s.btn_survey}>
-              설문하고
-              <br /> 추천받기
-            </a>
-          </Link>
-          <Link href={'/surveyGuide'} passHref>
+        <div className={s.video_wrapper}>
+          <video muted autoPlay loop>
+            <source src="/video/main-video.mp4" type="video/mp4" />
+          </video>
+          <div className={s.text_box}>
+            <h1>
+              우리는 사료가 아닌
+              <br /> 음식을 만듭니다
+            </h1>
+            <h2>보다 나은 견생을 위한 선택</h2>
+            <div className={s.image_wrap}>
+              <Image
+                src={require('/public/img/logo_main.png')}
+                alt="카드 이미지"
+              />
+            </div>
+            <div className={s.btn_box}>
+              <Link href={'/surveyGuide'} passHref>
+                <a type="button" className={s.btn_survey}>
+                  설문하고
+                  <br /> 추천받기
+                </a>
+              </Link>
+              {/* <Link href={'/surveyGuide'} passHref>
             <a type="button" className={s.btn_score}>
               건강점수 <br />
               비교하기
             </a>
-          </Link>
+          </Link> */}
+            </div>
+          </div>
         </div>
       </div>
     </section>
