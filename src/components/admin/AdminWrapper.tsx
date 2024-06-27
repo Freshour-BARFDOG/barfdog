dcd7479d554d6cb962d4f457240ba5f668242ae9
saywhat <love7620640@naver.com>
2024-06-27 14:13:41 +0900

    Feat: [update] 메인헤더 및 관리자 레이아웃 변경

--- src/components/admin/AdminWrapper.tsx
+++ src/components/admin/AdminWrapper.tsx
@@ -1,25 +1,30 @@
-import React from "react";
-import s from "./adminWrapper.module.scss";
+import React from 'react';
+import s from './adminWrapper.module.scss';
 
-
-export const AdminContentWrapper = ( {className, ...props }) => {
+export const AdminContentWrapper = ({ className, ...props }) => {
   return (
-    <div className={`${s.container_outer} ${s.contents_container_outer}`} {...props}>
+    <div
+      className={`${s.container_outer} ${s.contents_container_outer}`}
+      {...props}
+    >
       <div className={s.contents_container}>
-        <div className={`${s.contents_row} ${className ? className : ''}`}>{props.children}</div>
+        <div className={`${s.contents_row} ${className ? className : ''}`}>
+          {props.children}
+        </div>
       </div>
     </div>
   );
 };
 
-
-export const AdminBodyWrapper = ({ children,  folded, }) => {
-  return <div className={`${s.body_container} ${folded ? s.folded : ''}`}>{children}</div>;
+export const AdminBodyWrapper = ({ children, folded }) => {
+  return (
+    <div className={`${s.body_container} ${folded ? s.folded : ''}`}>
+      {children}
+    </div>
+  );
 };
 
-
-
-export default function AdminWrapper({children}) {
+export default function AdminWrapper({ children }) {
   return (
     <div className={s.container_outer}>
       <div className={s.container}>
