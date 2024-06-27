85351b06fcfe3cadff0ea214c25ae29a2875d8f7
saywhat <love7620640@naver.com>
2024-06-27 14:13:41 +0900

    Feat: [update] 메인헤더 및 관리자 레이아웃 변경

--- src/components/header/MoveToAdminPageButton.jsx
+++ src/components/header/MoveToAdminPageButton.jsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
-import {URLPathClass} from "../../class/URLPathClass";
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import { URLPathClass } from '../../class/URLPathClass';
+import Link from 'next/link';
 
-
+//! [ 삭제 예장 ]
 export const MoveToAdminPageButton = () => {
   // const [adminPath, setAdminPath] = useState( "" );
   // const [adminPath2, setAdminPath2] = useState( "" );
@@ -12,18 +12,21 @@ export const MoveToAdminPageButton = () => {
   //   const p = new URLPathClass();
   //   const path = p.lastVisitedAdminPath || ADMIN_INDEX_PATH;
   //   setAdminPath( path );
-    
+
   //   const ADMIN_INDEX_PATH2 = "/bf-admin2";
   //   const path2 = p.lastVisitedAdminPath || ADMIN_INDEX_PATH2;
   //   setAdminPath2( path2 );
   // }, [] );
-  
-  
+
   return (
     <>
-      <li><Link href="/bf-admin/dashboard">관리자 페이지</Link></li>
-      <li> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; </li>
-      <li><Link href="/bf-admin2">관리자 페이지2</Link></li>
+      <li>
+        <Link href="/bf-admin/dashboard">관리자 페이지</Link>
+      </li>
+      {/* <li> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; </li> */}
+      {/* <li>
+        <Link href="/bf-admin2">관리자 페이지2</Link>
+      </li> */}
     </>
   );
-}
+};
