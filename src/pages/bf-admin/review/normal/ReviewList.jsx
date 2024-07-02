a0b705099d07122638091becdc06d659e3f58191
saywhat <love7620640@naver.com>
2024-07-02 14:24:37 +0900

    Feat: [update] 관리자>리뷰>제목설정, 메인 리뷰제목 텍스트 추가, 레이아웃 수정
    1. 관리자>리뷰>제목설정
    - 베스트리뷰 선정 시 제목 없으면 alert창
    - 제목 조회 및 수정 가능
    2. 메인
    - 관리자 제목 반영
    - 그에 맞게 레이아웃 수정

--- src/pages/bf-admin/review/normal/ReviewList.jsx
+++ src/pages/bf-admin/review/normal/ReviewList.jsx
@@ -1,15 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import s from './review.module.scss';
 import Checkbox from '/src/components/atoms/Checkbox';
 import transformDate from '/util/func/transformDate';
 import popupWindow from '/util/func/popupWindow';
 import Link from 'next/link';
 import { global_reviewStateType } from '/store/TYPE/reviewStateType';
-import Spinner from "/src/components/atoms/Spinner";
-import extractPartOfURL from "/util/func/extractPartOfURL";
+import Spinner from '/src/components/atoms/Spinner';
+import extractPartOfURL from '/util/func/extractPartOfURL';
 
-export default function ReviewList({ items,onDeleteItem,
-                                     isLoading, selectedItems, setSelectedItems }) {
+export default function ReviewList({
+  items,
+  onDeleteItem,
+  onPostTitleItem,
+  isLoading,
+  selectedItems,
+  setSelectedItems,
+}) {
   if (!items || !items.length) return;
 
   return (
@@ -19,6 +25,7 @@ export default function ReviewList({ items,onDeleteItem,
           key={`item-${item.id}`}
           item={item}
           onDeleteItem={onDeleteItem}
+          onPostTitleItem={onPostTitleItem}
           isLoading={isLoading}
           setSelectedItems={setSelectedItems}
           selectedItems={selectedItems}
@@ -28,22 +35,27 @@ export default function ReviewList({ items,onDeleteItem,
   );
 }
 
-const ItemList = ({ item,
-                    onDeleteItem,
-                    isLoading,
-                    selectedItems,
-                    setSelectedItems
+const ItemList = ({
+  item,
+  onDeleteItem,
+  onPostTitleItem,
+  isLoading,
+  selectedItems,
+  setSelectedItems,
 }) => {
-
   let itemStatus;
   global_reviewStateType.forEach((type) => {
     if (type.ENG === item.status) {
       itemStatus = type.KOR;
     }
   });
-  
-  const [submittedDeleteApi, setSubmittedDeleteApi] = useState( false );
-  
+
+  const [submittedDeleteApi, setSubmittedDeleteApi] = useState(false);
+  const [titleValue, setTitleValue] = useState({
+    id: item.id,
+    titleByAdmin: item.titleByAdmin,
+  });
+
   const DATA = {
     id: item.id,
     status: itemStatus,
@@ -54,18 +66,25 @@ const ItemList = ({ item,
     email: item.email,
     createdDate: transformDate(item.createdDate) || '-',
     apiurl: {
+      postTitle: '/api/admin/reviews/title',
       delete: item._links?.delete_review.href,
     },
+    titleByAdmin: item.titleByAdmin,
   };
-  
+
   const onDelete = async (e) => {
-    if ( submittedDeleteApi ) return console.error ( "이미 제출된 양식입니다." );
-    if ( !confirm( `선택된 회원(${item.name})님의 리뷰를 삭제하시겠습니까?` ) ) return;
+    if (submittedDeleteApi) return console.error('이미 제출된 양식입니다.');
+    if (!confirm(`선택된 회원(${item.name})님의 리뷰를 삭제하시겠습니까?`))
+      return;
     const apiUrl = e.currentTarget.dataset.apiUrl;
-    onDeleteItem( extractPartOfURL( apiUrl ).pathname, DATA.id );
-    setSubmittedDeleteApi( true );
+    onDeleteItem(extractPartOfURL(apiUrl).pathname, DATA.id);
+    setSubmittedDeleteApi(true);
   };
-  
+
+  const onPostTitle = async (e) => {
+    const apiUrl = e.currentTarget.dataset.apiUrl;
+    onPostTitleItem(apiUrl, titleValue);
+  };
 
   const onPopupHandler = (e) => {
     e.preventDefault();
@@ -79,11 +98,23 @@ const ItemList = ({ item,
     if (checked) {
       setSelectedItems((prevState) => prevState.concat(selectedId));
     } else {
-      setSelectedItems((prevState) => prevState.filter((id) => id !== selectedId));
+      setSelectedItems((prevState) =>
+        prevState.filter((id) => id !== selectedId),
+      );
     }
   };
 
-  //
+  //* 제목 입력
+  const onInputChangeHandler = (e, id) => {
+    const input = e.currentTarget;
+    const { value } = input;
+
+    setTitleValue({
+      id,
+      titleByAdmin: value,
+    });
+  };
+
   return (
     <li className={s.item} key={`item-${DATA.id}`} data-idx={DATA.id}>
       <span>
@@ -114,12 +145,39 @@ const ItemList = ({ item,
       </span>
       <span>{DATA.createdDate}</span>
       <span>
+        {' '}
+        <input
+          id={'titleByAdmin'}
+          type="text"
+          name="titleByAdmin"
+          className={'text-align-right'}
+          placeholder="베스트리뷰 제목"
+          value={titleValue.titleByAdmin || ''}
+          onChange={(e) => onInputChangeHandler(e, DATA.id)}
+        />
+        <button
+          className="admin_btn basic_s solid"
+          onClick={onPostTitle}
+          data-api-url={DATA.apiurl.postTitle}
+        >
+          {isLoading?.post && isLoading.post[DATA.id] ? (
+            <Spinner style={{ color: 'white' }} />
+          ) : (
+            '등록'
+          )}
+        </button>
+      </span>
+      <span>
         <button
           className="admin_btn basic_s solid"
           onClick={onDelete}
           data-api-url={DATA.apiurl.delete}
         >
-          {(isLoading?.delete && isLoading.delete[DATA.id]) ? <Spinner style={{color: "white"}}/> : "삭제"}
+          {isLoading?.delete && isLoading.delete[DATA.id] ? (
+            <Spinner style={{ color: 'white' }} />
+          ) : (
+            '삭제'
+          )}
         </button>
       </span>
     </li>
