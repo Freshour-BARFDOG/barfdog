5d2e79d9d0bdae375ac435a677c953d3cc126cbb
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-10-17 15:13:35 +0900

    feat: vercel test

--- src/pages/api/axios/axios.config.js
+++ src/pages/api/axios/axios.config.js
@@ -10,7 +10,6 @@ axios.defaults.baseURL =
 axios.defaults.timeout = 10000;
 
 // console.log('axios.defaults.baseURL: ',axios.defaults.baseURL);
-
 // * axiosConfig > 유저 & ADMIN의 config이 동일하다 => 서버에서 토큰의 권한을 자체 검증하기 떄문에
 // CLIENT에서 아래와 같이 처리할 필요가 없다
 // 아래 코드 중에서 아무거나 골라써도 무방하다.
