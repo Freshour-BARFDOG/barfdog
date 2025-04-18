6986f92dc4fec2016b5d8574b72784ccc48bc3cd
jkeFreshour <jke@freshour.co.kr>
2025-04-18 13:14:50 +0900

    feat: 특수문자 제어 함수 하이픈(-) 제거 (상세 주소 입력 Input 이슈)

--- util/func/filter_specialCharacter.js
+++ util/func/filter_specialCharacter.js
@@ -1,7 +1,7 @@
 
 const filter_specialCharacter = (str) => {
   let filteredStr;
-  const reg = /[\{\}\[\]\/?.,;:|*~`!^\-_+<>@\#$%&\\\=\'\"]/gi;
+  const reg = /[\{\}\[\]\/?.,;:|*~`!^\_+<>@\#$%&\\\=\'\"]/gi;
   filteredStr = reg.test(str) ? str.replace(reg, "") : str;
 
   return filteredStr;
